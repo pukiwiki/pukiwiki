90ad7b02b03295ab2b008ac4fc504689a073c230
masui <masui>
2002-06-26 14:44:37 +0900

    [[pukiwiki:BugTrack/22]] DTDを追加

--- skin/pukiwiki.skin.en.php
+++ skin/pukiwiki.skin.en.php
@@ -1,4 +1,5 @@
 <? global $page_title; ?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <title><?=$page_title ?> - <?=$title?></title>
