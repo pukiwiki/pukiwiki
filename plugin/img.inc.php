f5c11576e70090a9a98924d1688f172efa2a9cf2
masui <masui>
2002-07-02 14:38:17 +0900

    [[pukiwiki:BugTrack/52]] ファイル名チェックを強化

--- plugin/img.inc.php
+++ plugin/img.inc.php
@@ -1,6 +1,5 @@
 <?
-// $Id: img.inc.php,v 1.3 2002/07/01 07:08:57 masui Exp $
-
+// $Id: img.inc.php,v 1.4 2002/07/02 05:38:17 masui Exp $
 function plugin_img_convert()
 {
 	if(func_num_args()!=2) {
@@ -9,7 +8,6 @@ function plugin_img_convert()
 	$aryargs = func_get_args();
 	$url = $aryargs[0];
 	$align = strtoupper($aryargs[1]);
-	
 	if($align == 'R' || $align == 'RIGHT') {
 		$align = 'right';
 	}
@@ -19,6 +17,7 @@ function plugin_img_convert()
 	else {
 		return "<br style=\"clear:both\">";
 	}
+	if(preg_match("/^http:\/\/(\S+?)(\.jpg|\.jpeg|\.gif|\.png)$/si", $url) == false) return;
 	return "<div style=\"float:$align;padding:.5em 1.5em .5em 1.5em\"><img src=\"$url\" alt=\"\" /></div>";
 }
 ?>
