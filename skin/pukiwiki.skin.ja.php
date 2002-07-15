8dffaf5cc3e3204bb927abd40897a09fbf90244e
masui <masui>
2002-07-15 16:24:58 +0900

    <!DOCTYPE>のケースを修正

--- skin/pukiwiki.skin.ja.php
+++ skin/pukiwiki.skin.ja.php
@@ -1,5 +1,5 @@
 <? global $page_title; header("Content-Type: text/html; charset=euc-jp") ?>
-<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=euc-jp">
