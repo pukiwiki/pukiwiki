f8a633bb35947fa2b89419f94db15a83469fc8f8
panda <panda>
2002-12-05 14:41:43 +0900

    BracketNameの扱いを調整

--- plugin/navi.inc.php
+++ plugin/navi.inc.php
@@ -1,7 +1,7 @@
 <?php
-// $Id: navi.inc.php,v 1.3 2002/12/05 05:02:27 panda Exp $
+// $Id: navi.inc.php,v 1.4 2002/12/05 05:41:43 panda Exp $
 /*
-Last-Update:2002-10-15 rev.2
+Last-Update:2002-12-05 rev.3
 
 *プラグイン navi
 
@@ -18,7 +18,7 @@ DobBook風のナビゲーションバーを表示する
 
 *動作
 
-1ページで2回呼ばれることを考慮して、関連変数はグローバルに持つ。
+1ページで2回呼ばれることを考慮して、関連変数はスタティックに持つ。
 
 -1回目の参照(HOME)~
  ページ一覧をls.inc.php風に表示する
@@ -34,33 +34,36 @@ DobBook風のナビゲーションバーを表示する
 
 */
 function plugin_navi_convert() {
-	global $vars, $script;
-	global $_navi_pages;
+	global $script,$vars,$WikiName,$BracketName;
+	static $_navi_pages;
 
-	$home = $vars[page];
+	$home = $vars['page'];
 	if (func_num_args()) {
-		$args = func_get_args();
-		$home = $args[0];
+		list($home) = func_get_args();
+		if (!preg_match("/^($WikiName|$BracketName)$/",$home))
+			$home = "[[$home]]";
+		if (!is_page($home))
+			return 'no page.';
 	}
-	$is_home = ($home == $vars[page]);
-	$current = strip_bracket($vars[page]);
+	$is_home = ($home == $vars['page']);
+	$current = strip_bracket($vars['page']);
 
 	$pattern = encode('[['.strip_bracket($home).'/');
 	$length = strlen($pattern);
 
-	$footer = is_array($_navi_pages);
+	$footer = isset($_navi_pages);
 	if (!$footer) {
 		if ($dir = @opendir(DATA_DIR)) {
 			while ($name = readdir($dir)) {
-				if ($name == ".." || $name == ".") { continue; }
+				if ($name == '..' || $name == '.') { continue; }
 				if (strpos($name, $pattern) === 0) {
-					$pages[] = strip_bracket(decode(trim(preg_replace("/\.txt$/"," ",$name))));
+					$pages[] = strip_bracket(decode(trim(preg_replace('/\.txt$/',' ',$name))));
 				}
 			}
 			closedir($dir);
 		}
 		// 未作成時のための番兵(プレビューとか)
-		if (array_search($current,$pages) === false) { $pages[] = $current; }
+		if (array_search($current,$pages) === FALSE) { $pages[] = $current; }
 		natcasesort($pages);
 		$prev = $home;
 		foreach ($pages as $page) {
@@ -70,49 +73,49 @@ function plugin_navi_convert() {
 		$next = current($pages);
 
 		$_navi_pages = array(
-			"up" => "&nbsp;",
-			"home" => "&nbsp;",
-			"home1" => "&nbsp;",
-			"prev" => "&nbsp;",
-			"prev1" => "&nbsp;",
-			"next" => "&nbsp;",
-			"next1" => "&nbsp;",
+			'up' => '&nbsp;',
+			'home' => '&nbsp;',
+			'home1' => '&nbsp;',
+			'prev' => '&nbsp;',
+			'prev1' => '&nbsp;',
+			'next' => '&nbsp;',
+			'next1' => '&nbsp;',
 		);
 
-		$pos = strrpos($current, "/");
+		$pos = strrpos($current, '/');
 		if ($pos > 0) {
-			$_navi_pages[up] = make_link("[[Up&gt;".substr($current, 0, $pos)."]]");
+			$_navi_pages['up'] = make_link('[[Up&gt;'.substr($current, 0, $pos).']]');
 		}
 		if (!$is_home) {
 			if ($prev != $home) {
-				$_navi_pages[prev] = make_link("[[$prev]]");
+				$_navi_pages['prev'] = make_link("[[$prev]]");
 			} else {
-				$_navi_pages[prev] = make_link("$prev");
+				$_navi_pages['prev'] = make_link("$prev");
 			}
-			$_navi_pages[prev1] = make_link("[[Prev&gt;$prev]]");
+			$_navi_pages['prev1'] = make_link("[[Prev&gt;$prev]]");
 		}
 		if ($next != "") {
-			$_navi_pages[next] = make_link("[[$next]]");
-			$_navi_pages[next1] = make_link("[[Next&gt;$next]]");
+			$_navi_pages['next'] = make_link("[[$next]]");
+			$_navi_pages['next1'] = make_link("[[Next&gt;$next]]");
 		}
 		if (!$is_home) {
-			$_navi_pages[home] = make_link($home);
-			$_navi_pages[home1] = make_link(preg_replace("/^(\[\[)?/","$1Home&gt;",$home));
+			$_navi_pages['home'] = make_link($home);
+			$_navi_pages['home1'] = make_link(preg_replace("/^(\[\[)?/","$1Home&gt;",$home));
 		}
 	}
 
-	$ret = "";
+	$ret = '';
 	if ($footer) { //フッタ
 		$ret = <<<EOD
 <div class=".navi_footer">
 <hr width="100%">
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td width="33%" align="left" valign="top">$_navi_pages[prev1]</td>
-<td width="34%" align="center" valign="top">$_navi_pages[home1]</td>
-<td width="33%" align="right" valign="top">$_navi_pages[next1]</td></tr>
-<tr><td width="33%" align="left" valign="top">$_navi_pages[prev]</td>
-<td width="34%" align="center" valign="top">$_navi_pages[up]</td>
-<td width="33%" align="right" valign="top">$_navi_pages[next]</td></tr>
+<tr><td width="33%" align="left" valign="top">{$_navi_pages['prev1']}</td>
+<td width="34%" align="center" valign="top">{$_navi_pages['home1']}</td>
+<td width="33%" align="right" valign="top">{$_navi_pages['next1']}</td></tr>
+<tr><td width="33%" align="left" valign="top">{$_navi_pages['prev']}</td>
+<td width="34%" align="center" valign="top">{$_navi_pages['up']}</td>
+<td width="33%" align="right" valign="top">{$_navi_pages['next']}</td></tr>
 </table>
 </div>
 EOD;
@@ -127,9 +130,9 @@ EOD;
 		$ret = <<<EOD
 <div class=".navi_header">
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td width="33%" align="left" valign="top">$_navi_pages[prev1]</td>
-<td width="34%" align="center" valign="top">$_navi_pages[home]</td>
-<td width="33%" align="right" valign="top">$_navi_pages[next1]</td></tr>
+<tr><td width="33%" align="left" valign="top">{$_navi_pages['prev1']}</td>
+<td width="34%" align="center" valign="top">{$_navi_pages['home']}</td>
+<td width="33%" align="right" valign="top">{$_navi_pages['next1']}</td></tr>
 </table>
 <hr width="100%">
 </div>
