64e1ce9bebc41de8503d286984d6aa26ae6da2f9
panda <panda>
2003-02-26 12:04:25 +0900

    add plugin name check (\w{1,64}).

--- plugin.php
+++ plugin.php
@@ -2,50 +2,62 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: plugin.php,v 1.3 2003/01/27 05:38:44 panda Exp $
+// $Id: plugin.php,v 1.4 2003/02/26 03:04:25 panda Exp $
 //
 
 // プラグイン用に未定義の変数を設定
 function set_plugin_messages($messages)
 {
-	foreach ($messages as $name=>$val) {
+	foreach ($messages as $name=>$val)
+	{
 		global $$name;
 		
-		if(!isset($$name)) {
+		if(!isset($$name))
+		{
 			$$name = $val;
 		}
 	}
 }
 
+//プラグインが存在するか
+function exist_plugin($name)
+{
+	if (preg_match('/^\w{1,64}$/',$name)
+		and file_exists(PLUGIN_DIR.$name.'.inc.php'))
+	{
+		require_once(PLUGIN_DIR.$name.'.inc.php');
+		return TRUE;
+	}
+	return FALSE;
+}
+
 //プラグイン(action)が存在するか
 function exist_plugin_action($name)
 {
-	if (!file_exists(PLUGIN_DIR.$name.'.inc.php')) {
+	if (!exist_plugin($name))
+	{
 		return FALSE;
 	}
-	
-	require_once(PLUGIN_DIR.$name.'.inc.php');
 	return function_exists('plugin_'.$name.'_action');
 }
 
 //プラグイン(convert)が存在するか
-function exist_plugin_convert($name) {
-	if (!file_exists(PLUGIN_DIR.$name.'.inc.php')) {
+function exist_plugin_convert($name)
+{
+	if (!exist_plugin($name))
+	{
 		return FALSE;
 	}
-	
-	require_once(PLUGIN_DIR.$name.'.inc.php');
 	return function_exists('plugin_'.$name.'_convert');
 }
 
 //プラグイン(inline)が存在するか
 function exist_plugin_inline($name)
 {
-	if (!file_exists(PLUGIN_DIR.$name.'.inc.php')) {
+	if (!exist_plugin($name))
+	{
 		return FALSE;
 	}
-	
-	require_once(PLUGIN_DIR.$name.'.inc.php');
 	return function_exists('plugin_'.$name.'_inline');
 }
 
@@ -60,7 +72,8 @@ function do_plugin_init($name)
 	$func_check = '_funccheck_'.$funcname;
 	global $$func_check;
 	
-	if ($$func_check) {
+	if ($$func_check)
+	{
 		return TRUE;
 	}
 	$$func_check = TRUE;
@@ -70,7 +83,8 @@ function do_plugin_init($name)
 //プラグイン(action)を実行
 function do_plugin_action($name)
 {
-	if(!exist_plugin_action($name)) {
+	if(!exist_plugin_action($name))
+	{
 		return array();
 	}
 	
@@ -86,7 +100,8 @@ function do_plugin_convert($name,$args)
 	do_plugin_init($name);
 	$retvar = call_user_func_array('plugin_'.$name.'_convert',$aryargs);
 	
-	if($retvar === FALSE) {
+	if($retvar === FALSE)
+	{
 		return "#${name}(${args})";
 	}
 	
@@ -102,7 +117,8 @@ function do_plugin_inline($name,$args,$body)
 	do_plugin_init($name);
 	$retvar = call_user_func_array('plugin_'.$name.'_inline',$aryargs);
 	
-	if($retvar === FALSE) {
+	if($retvar === FALSE)
+	{
 		return "#${name}(${args})";
 	}
 	
