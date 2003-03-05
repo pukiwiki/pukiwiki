69260261a91c32964babdcda2b5adba03d2bf110
panda <panda>
2003-03-05 15:58:49 +0900

    add function md5_file() for PHP<4.2.0. (use in attach.inc.php :) )

--- func.php
+++ func.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: func.php,v 1.24 2003/03/04 07:05:00 panda Exp $
+// $Id: func.php,v 1.25 2003/03/05 06:58:49 panda Exp $
 //
 
 // 文字列がInterWikiNameかどうか
@@ -512,14 +512,17 @@ function sanitize_null_character($param)
 //
 //is_a --  Returns TRUE if the object is of this class or has this class as one of its parents 
 
-if (!function_exists('is_a')) {
+if (!function_exists('is_a'))
+{
 	function is_a($class, $match)
 	{
-		if (empty($class)) {
+		if (empty($class))
+		{
 			return false;
 		}
 		$class = is_object($class) ? get_class($class) : $class;
-		if (strtolower($class) == strtolower($match)) {
+		if (strtolower($class) == strtolower($match))
+		{
 			return true;
 		}
 		return is_a(get_parent_class($class), $match);
@@ -531,14 +534,37 @@ if (!function_exists('is_a')) {
 //
 //array_fill -- Fill an array with values
 
-if (!function_exists('array_fill')) {
-	function array_fill($start_index,$num,$value) {
+if (!function_exists('array_fill'))
+{
+	function array_fill($start_index,$num,$value)
+	{
 		$ret = array();
 		
 		while ($num-- > 0)
+		{
 			$ret[$start_index++] = $value;
-		
+		}
 		return $ret;
+	}
+}
+//md5_file
+//(PHP 4 >= 4.2.0)
+//
+//md5_file -- Calculates the md5 hash of a given filename
+
+if (!function_exists('md5_file'))
+{
+	function md5_file($filename)
+	{
+		
+		if (!file_exists($filename))
+		{
+			return FALSE;
+		}
+		$fd = fopen($filename, 'rb');
+		$data = fread($fd, filesize($filename));
+		fclose($fd);
+		return md5($data);
 	}
 }
 ?>
