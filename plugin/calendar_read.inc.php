1ebdf0af31b5bd0e8d7b16356a64aefd207deed1
panda <panda>
2003-03-08 21:32:06 +0900

    bugfix: func_num_args(): Can't be used as a function parameter.

--- plugin/calendar_read.inc.php
+++ plugin/calendar_read.inc.php
@@ -1,5 +1,9 @@
 <?php
-// $Id: calendar_read.inc.php,v 1.5 2003/01/27 05:38:44 panda Exp $
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: calendar_read.inc.php,v 1.6 2003/03/08 12:32:06 panda Exp $
+//
 
 function plugin_calendar_read_convert()
 {
@@ -7,13 +11,17 @@ function plugin_calendar_read_convert()
 	
 	$command = 'read';
 	
-	if (!file_exists(PLUGIN_DIR.'calendar.inc.php')) {
+	if (!file_exists(PLUGIN_DIR.'calendar.inc.php'))
+	{
 		return FALSE;
 	}
 	require_once PLUGIN_DIR.'calendar.inc.php';
+	if (!function_exists('plugin_calendar_convert'))
+	{
+		return FALSE;
+	}
 	
-	return call_user_func_array('plugin_calendar_convert',
-		func_num_args() ? func_get_args() : array()
-	);
+	$args = func_num_args() ? func_get_args() : array();
+	return call_user_func_array('plugin_calendar_convert',$args);
 }
 ?>
