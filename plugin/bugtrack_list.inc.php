0f8dadae65d1d241519451537efc4cf5ba79ebf2
panda <panda>
2003-03-12 12:35:53 +0900

    BugTrack/260:use PLUGIN_DIR.

--- plugin/bugtrack_list.inc.php
+++ plugin/bugtrack_list.inc.php
@@ -8,10 +8,10 @@
  * 変更履歴:
  *  2002.06.17: 作り始め
  *
- * $Id: bugtrack_list.inc.php,v 1.2 2002/12/05 05:45:18 panda Exp $
+ * $Id: bugtrack_list.inc.php,v 1.2.2.1 2003/03/12 03:35:53 panda Exp $
  */
 
-require_once("plugin/bugtrack.inc.php");
+require_once(PLUGIN_DIR.'bugtrack.inc.php');
 
 function plugin_bugtrack_list_init() {
   plugin_bugtrack_init();
