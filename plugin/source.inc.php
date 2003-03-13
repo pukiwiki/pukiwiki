41d466dc6b5461a6cc9f5d6829fb4f1346ddd09a
panda <panda>
2003-03-13 15:03:15 +0900

    BugTrack/258:fix XSS vulnerability. add error check and messages.

--- plugin/source.inc.php
+++ plugin/source.inc.php
@@ -1,14 +1,43 @@
 <?php
-// $Id: source.inc.php,v 1.4 2002/12/05 05:02:27 panda Exp $
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: source.inc.php,v 1.4.2.1 2003/03/13 06:03:15 panda Exp $
+//
+// ページソースを表示
+
+function plugin_source_init()
+{
+	$messages = array(
+		'_source_messages'=>array(
+			'msg_title' => '$1のソース',
+			'msg_notfound' => '$1が見つかりません',
+			'err_notfound' => 'ページのソースを表示できません。'
+		)
+	);
+	set_plugin_messages($messages);
+}
 
 function plugin_source_action()
 {
 	global $vars;
-
-	header('Content-type: text/plain');
-	readfile(get_filename(encode($vars['page'])));
-
-	die();
+	global $_source_messages;
+	
+	$vars['refer'] = $vars['page'];
+	
+	if (!is_page($vars['page']))
+	{
+		return array(
+			'msg'=>$_source_messages['msg_notfound'],
+			'body'=>$_source_messages['err_notfound']
+		);
+	}
+	return array(
+		'msg'=>$_source_messages['msg_title'],
+		'body' =>
+			'<pre id="source">'.
+			htmlspecialchars(join('',get_source($vars['page']))).
+			'</pre>'
+	);
 }
-
 ?>
