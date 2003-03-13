f3ed5e366d99c739446c3d00149ffcc99dc9b649
panda <panda>
2003-03-13 14:31:16 +0900

    BugTrack/258:fix XSS vulnerability.

--- plugin/source.inc.php
+++ plugin/source.inc.php
@@ -1,14 +1,32 @@
 <?php
-// $Id: source.inc.php,v 1.6 2003/02/18 04:30:32 panda Exp $
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: source.inc.php,v 1.7 2003/03/13 05:31:16 panda Exp $
+//
+// ページソースを表示
+
+function plugin_source_init()
+{
+	$messages = array(
+		'_source_messages'=>array(
+		'msg_title' => '$1のソース'
+		)
+	);
+	set_plugin_messages($messages);
+}
 
 function plugin_source_action()
 {
 	global $vars;
+	global $_source_messages;
 	
-	header('Content-type: text/plain; charset="'.SOURCE_ENCODING.'"');
-	echo join('',get_source($vars['page']));
-	
-	die();
+	return array(
+		'msg'=>$_source_messages['msg_title'],
+		'body' =>
+			'<pre id="source">'.
+			htmlspecialchars(join('',get_source($vars['page']))).
+			'</pre>'
+	);
 }
-
 ?>
