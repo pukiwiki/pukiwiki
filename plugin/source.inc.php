b07ffe29767b92c6711085c5623262391acf4f2b
panda <panda>
2003-03-13 15:00:23 +0900

    add error check and messages.

--- plugin/source.inc.php
+++ plugin/source.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: source.inc.php,v 1.7 2003/03/13 05:31:16 panda Exp $
+// $Id: source.inc.php,v 1.8 2003/03/13 06:00:23 panda Exp $
 //
 // ページソースを表示
 
@@ -10,7 +10,9 @@ function plugin_source_init()
 {
 	$messages = array(
 		'_source_messages'=>array(
-		'msg_title' => '$1のソース'
+			'msg_title' => '$1のソース',
+			'msg_notfound' => '$1が見つかりません',
+			'err_notfound' => 'ページのソースを表示できません。'
 		)
 	);
 	set_plugin_messages($messages);
@@ -21,6 +23,15 @@ function plugin_source_action()
 	global $vars;
 	global $_source_messages;
 	
+	$vars['refer'] = $vars['page'];
+	
+	if (!is_page($vars['page']))
+	{
+		return array(
+			'msg'=>$_source_messages['msg_notfound'],
+			'body'=>$_source_messages['err_notfound']
+		);
+	}
 	return array(
 		'msg'=>$_source_messages['msg_title'],
 		'body' =>
