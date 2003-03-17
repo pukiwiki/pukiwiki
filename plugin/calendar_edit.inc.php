ce8207341ef0ce852bb6c1b749bcecbcef615fcd
panda <panda>
2003-03-17 09:20:38 +0900

    BugTrack/266:bugfix.

--- plugin/calendar_edit.inc.php
+++ plugin/calendar_edit.inc.php
@@ -1,19 +1,27 @@
 <?php
-// $Id: calendar_edit.inc.php,v 1.5 2003/01/27 05:38:44 panda Exp $
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: calendar_edit.inc.php,v 1.6 2003/03/17 00:20:38 panda Exp $
+//
 
-function plugin_calendar_read_convert()
+function plugin_calendar_edit_convert()
 {
 	global $command;
 	
 	$command = 'edit';
 	
-	if (!file_exists(PLUGIN_DIR.'calendar.inc.php')) {
+	if (!file_exists(PLUGIN_DIR.'calendar.inc.php'))
+	{
 		return FALSE;
 	}
 	require_once PLUGIN_DIR.'calendar.inc.php';
+	if (!function_exists('plugin_calendar_convert'))
+	{
+		return FALSE;
+	}
 	
-	return call_user_func_array('plugin_calendar_convert',
-		func_num_args() ? func_get_args() : array()
-	);
+	$args = func_num_args() ? func_get_args() : array();
+	return call_user_func_array('plugin_calendar_convert',$args);
 }
 ?>
