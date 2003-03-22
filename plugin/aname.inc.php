e1e2fda9638d352793f7d24a697873cfa6793ed4
panda <panda>
2003-03-23 00:12:35 +0900

    BugTrack/270:fix XSS vulnerability.

--- plugin/aname.inc.php
+++ plugin/aname.inc.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: aname.inc.php,v 1.6 2002/12/05 05:50:41 panda Exp $
+// $Id: aname.inc.php,v 1.6.2.1 2003/03/22 15:12:35 panda Exp $
 
 function plugin_aname_convert()
 {
@@ -8,6 +8,6 @@ function plugin_aname_convert()
   if (eregi("^[A-Z][A-Z0-9\-_]*$", $aryargs[0]))
     return "<a name=\"$aryargs[0]\" id=\"$aryargs[0]\"></a>";
   else
-    return "Bad Aname!! -- ".$aryargs[0]."\n";
+    return "Bad Aname!! -- ".htmlspecialchars($aryargs[0])."\n";
 }
 ?>
