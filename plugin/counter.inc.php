72e28a449d7fa1370c78f73ff31b2bbcd3eb4da6
panda <panda>
2003-04-02 13:16:01 +0900

    BugTrack/279:store counter cache to array for 'each page'.

--- plugin/counter.inc.php
+++ plugin/counter.inc.php
@@ -5,15 +5,17 @@
  * CopyRight 2002 Y.MASUI GPL2
  * http://masui.net/pukiwiki/ masui@masui.net
  *
- * $Id: counter.inc.php,v 1.6 2003/01/27 05:38:44 panda Exp $
+ * $Id: counter.inc.php,v 1.7 2003/04/02 04:16:01 panda Exp $
  */
 
 // counter file
-if (!defined('COUNTER_DIR')) {
+if (!defined('COUNTER_DIR'))
+{
 	define('COUNTER_DIR', './counter/');
 }
 
-if (!defined('COUNTER_EXT')) {
+if (!defined('COUNTER_EXT'))
+{
 	define('COUNTER_EXT','.count');
 }
 
@@ -26,16 +28,19 @@ function plugin_counter_convert()
 function plugin_counter_inline()
 {
 	$arg = '';
-	if (func_num_args() > 0) {
+	if (func_num_args() > 0)
+	{
 		list($arg) = func_get_args();
 	}
 	
 	list($count,$today_count,$yesterday_count) = plugin_counter_count();
 	
-	if ($arg == 'today') {
+	if ($arg == 'today')
+	{
 		return $today_count;
 	}
-	else if ($arg == 'yesterday') {
+	else if ($arg == 'yesterday')
+	{
 		return $yesterday_count;
 	}
 	return $count;
@@ -43,19 +48,30 @@ function plugin_counter_inline()
 function plugin_counter_count()
 {
 	global $vars,$HTTP_SERVER_VARS;
-	static $_counter_count;
+	static $counters;
 	
-	if (!is_page($vars['page'])) {
+	$page = $vars['page'];
+	
+	if (!is_page($page))
+	{
 		return array(0,0,0);
 	}
-	if (isset($_counter_count)) {
-		return $_counter_count;
+	if (!isset($counters))
+	{
+		$counters = array();
+	}
+	if (array_key_exists($page,$counters))
+	{
+		return $counters[$page];
 	}
 	
-	$file = COUNTER_DIR.encode($vars['page']).COUNTER_EXT;
-	if (!file_exists($file)) {
+	$file = COUNTER_DIR.encode($page).COUNTER_EXT;
+	if (!file_exists($file))
+	{
 		$nf = fopen($file, 'w');
+		flock($nf,LOCK_EX);
 		fputs($nf,"0\n0\n0\n0\n\n");
+		flock($nf,LOCK_UN);
 		fclose($nf);
 	}
 	
@@ -67,9 +83,11 @@ function plugin_counter_count()
 	$ip = rtrim($array[4]);
 	
 	//前回とIPアドレスが異なった場合にカウンタを回す
-	if ($ip != $HTTP_SERVER_VARS['REMOTE_ADDR']) {
+	if ($ip != $HTTP_SERVER_VARS['REMOTE_ADDR'])
+	{
 		$t = get_date('Y/m/d');
-		if ($t != $today) {
+		if ($t != $today)
+		{
 			$yesterday_count = $today_count;
 			$today_count = 0;
 			$today = $t;
@@ -78,7 +96,8 @@ function plugin_counter_count()
 		++$today_count;
 	}
 	//ページ読み出し時のみファイルを更新
-	if ($vars['cmd'] == 'read') {
+	if ($vars['cmd'] == 'read')
+	{
 		$ip = $HTTP_SERVER_VARS['REMOTE_ADDR'];
 		$nf = fopen($file, 'w');
 		flock($nf,LOCK_EX);
@@ -91,8 +110,8 @@ function plugin_counter_count()
 		fclose($nf);
 	}
 	
-	$_counter_count = array($count,$today_count,$yesterday_count);
+	$counters[$page] = array($count,$today_count,$yesterday_count);
 	
-	return $_counter_count;
+	return $counters[$page];
 }
 ?>
