0fa854c0d944563e81195af9200ef0384f38b9d5
arino <arino>
2003-04-25 11:29:28 +0900

    BugTrack/311:fix XSS vulnerability.

--- skin/pukiwiki.skin.en.php
+++ skin/pukiwiki.skin.en.php
@@ -1,3 +1,4 @@
+<?php if (!defined('DATA_DIR')) { exit; } ?>
 <?php global $page_title; header("Content-Type: text/html; charset=iso-8859-1") ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
