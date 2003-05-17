da8e794f1190cf30b8f809c712c76ffbc5980631
arino <arino>
2003-05-17 10:49:55 +0900

    BugTrack/332: nodateオプションが効かなくなっていた

--- plugin/comment.inc.php
+++ plugin/comment.inc.php
@@ -1,5 +1,9 @@
 <?php
-// $Id: comment.inc.php,v 1.17 2003/05/14 10:13:30 arino Exp $
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: comment.inc.php,v 1.18 2003/05/17 01:49:55 arino Exp $
+//
 
 /////////////////////////////////////////////////
 // コメントの名前テキストエリアのカラム数
@@ -30,56 +34,55 @@ function plugin_comment_action()
 	
 	$post['msg'] = preg_replace("/\n/",'',$post['msg']);
 	
-	if ($post['msg'] == '') {
+	if ($post['msg'] == '')
+	{
 		return array('msg'=>'','body'=>'');
 	}
 	
-	$comment_format = COMMENT_FORMAT;
-	if ($post['nodate']=='1') {
-		$comment_format = str_replace('$now','',$comment_format);
+	$head = '';
+	if (preg_match('/^(-{1,2})(.*)/',$post['msg'],$match))
+	{
+		$head = $match[1];
+		$post['msg'] = $match[2];
 	}
+	
+	$_msg  =                                 str_replace('$msg', $post['msg'], COMMENT_MSG_FORMAT);
+	$_name = ($post['name'] == '')    ? '' : str_replace('$name',$post['name'],COMMENT_NAME_FORMAT);
+	$_now  = ($post['nodate'] == '1') ? '' : str_replace('$now', $now,         COMMENT_NOW_FORMAT);
+	
+	$comment = str_replace("\x08MSG\x08", $_msg, COMMENT_FORMAT);
+	$comment = str_replace("\x08NAME\x08",$_name,$comment);
+	$comment = str_replace("\x08NOW\x08", $_now, $comment);
+	$comment = $head.$comment;
 	
 	$postdata = '';
 	$postdata_old  = get_source($post['refer']);
 	$comment_no = 0;
 	
-	$name = '';
-	if ($post['name'] != '') {
-		$name = str_replace('$name',$post['name'],COMMENT_NAME_FORMAT);
-	}
-	
-	$head = '';
-	if (preg_match('/^(-{1,2})(.*)/',$post['msg'],$match)) {
-		$head = $match[1];
-		$post['msg'] = $match[2];
-	}
-	
-	$comment = str_replace("\x08MSG\x08",str_replace('$msg',$post['msg'],COMMENT_MSG_FORMAT),$comment_format);
-	$comment = str_replace("\x08NAME\x08",$name,$comment);
-	$comment = str_replace("\x08NOW\x08",str_replace('$now',$now,COMMENT_NOW_FORMAT),$comment);
-	$comment = $head.$comment;
-	
-	foreach($postdata_old as $line) {
-		if (!COMMENT_INS) {
+	foreach ($postdata_old as $line)
+	{
+		if (!COMMENT_INS)
+		{
 			$postdata .= $line;
 		}
-		if (preg_match('/^#comment/',$line)) {
-			if ($comment_no == $post['comment_no'] and $post['msg'] != '') {
-				$postdata = rtrim($postdata)."\n-$comment\n";
-				if (COMMENT_INS) {
-					$postdata .= "\n";
-				}
+		if (preg_match('/^#comment/',$line) and $comment_no++ == $post['comment_no'])
+		{
+			$postdata = rtrim($postdata)."\n-$comment\n";
+			if (COMMENT_INS)
+			{
+				$postdata .= "\n";
 			}
-			$comment_no++;
 		}
-		if (COMMENT_INS) {
+		if (COMMENT_INS)
+		{
 			$postdata .= $line;
 		}
 	}
 	
 	$title = $_title_updated;
 	$body = '';
-	if (md5(@join('',get_source($post['refer']))) != $post['digest']) {
+	if (md5(@join('',get_source($post['refer']))) != $post['digest'])
+	{
 		$title = $_title_comment_collided;
 		$body = $_msg_comment_collided . make_pagelink($post['refer']);
 	}
@@ -107,10 +110,12 @@ function plugin_comment_convert()
 	
 	$options = func_num_args() ? func_get_args() : array();
 	
-	if (in_array('noname',$options)) {
+	if (in_array('noname',$options))
+	{
 		$nametags = $_msg_comment;
 	}
-	else {
+	else
+	{
 		$nametags = $_btn_name.'<input type="text" name="name" size="'.COMMENT_NAME_COLS."\" />\n";
 	}
 	
