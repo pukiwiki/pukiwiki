605804987ac5d4e4a771025f260746cd35b9cf33
arino <arino>
2003-06-05 19:38:03 +0900

    コードを整理

--- plugin/freeze.inc.php
+++ plugin/freeze.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: freeze.inc.php,v 1.3 2003/06/05 05:00:45 arino Exp $
+// $Id: freeze.inc.php,v 1.4 2003/06/05 10:38:03 arino Exp $
 //
 // 凍結
 function plugin_freeze_convert()
@@ -16,19 +16,22 @@ function plugin_freeze_action()
 	global $_title_isfreezed,$_title_freezed,$_title_freeze;
 	global $_msg_invalidpass,$_msg_freezing,$_btn_freeze;
 	
-	$msg = $body = '';
-	
-	if (!$function_freeze or !is_page($vars['page'])) {
-		return array('msg'=>$msg,'body'=>$body);
+	if (!$function_freeze or !is_page($vars['page']))
+	{
+		return array('msg'=>'','body'=>'');
 	}
 	
 	$pass = array_key_exists('pass',$post) ? $post['pass'] : NULL;
 	
-	if (is_freeze($vars['page'])) {
-		$msg = $_title_isfreezed;
-		$body = str_replace('$1',htmlspecialchars(strip_bracket($vars['page'])),$_title_isfreezed);
+	if (is_freeze($vars['page']))
+	{
+		return array(
+			'msg' => $_title_isfreezed,
+			'body' => str_replace('$1',htmlspecialchars(strip_bracket($vars['page'])),$_title_isfreezed)
+		);
 	}
-	else if (md5($pass) == $adminpass) {
+	else if (md5($pass) == $adminpass)
+	{
 		$postdata = get_source($post['page']);
 		array_unshift($postdata,"#freeze\n");
 		$postdata = join('',$postdata);
@@ -36,22 +39,14 @@ function plugin_freeze_action()
 		file_write(DATA_DIR,$vars['page'],$postdata,TRUE);
 		
 		$vars['cmd'] = 'read';
-		$msg = $_title_freezed;
-		$body = '';
+		return array('msg'=>$_title_freezed,'body'=>'');
 	}
-	else {
-		$msg = $_title_freeze;
-
-		$body = "<br />\n";
-		
-		if ($pass !== NULL) {
-			$body .= "<strong>$_msg_invalidpass</strong><br />\n";
-		}
-		
-		$body.= "$_msg_freezing<br />\n";
-		
-		$s_page = htmlspecialchars($vars['page']);
-		$body .= <<<EOD
+	// 凍結フォームを表示
+	$s_page = htmlspecialchars($vars['page']);
+	
+	$body = ($pass === NULL) ? '' : "<p><strong>$_msg_invalidpass</strong></p>\n";
+	$body .= <<<EOD
+<p>$_msg_freezing</p>
 <form action="$script" method="post">
  <div>
   <input type="hidden" name="cmd" value="freeze" />
@@ -61,8 +56,7 @@ function plugin_freeze_action()
  </div>
 </form>
 EOD;
-	}
 	
-	return array('msg'=>$msg,'body'=>$body);
+	return array('msg'=>$_title_freeze,'body'=>$body);
 }
 ?>
--- plugin/unfreeze.inc.php
+++ plugin/unfreeze.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: unfreeze.inc.php,v 1.3 2003/06/05 05:00:45 arino Exp $
+// $Id: unfreeze.inc.php,v 1.4 2003/06/05 10:38:03 arino Exp $
 //
 // 凍結解除
 
@@ -15,19 +15,19 @@ function plugin_unfreeze_action()
 	global $_title_isunfreezed,$_title_unfreezed,$_title_unfreeze;
 	global $_msg_invalidpass,$_msg_unfreezing,$_btn_unfreeze;
 	
-	$msg = $body = '';
-	
 	if (!$function_freeze or !is_page($vars['page']))
 	{
-		return array('msg'=>$msg,'body'=>$body);
+		return array('msg'=>'','body'=>'');
 	}
 	
 	$pass = array_key_exists('pass',$post) ? $post['pass'] : NULL;
 	
 	if (!is_freeze($vars['page']))
 	{
-		$msg = $_title_isunfreezed;
-		$body = str_replace('$1',htmlspecialchars(strip_bracket($vars['page'])),$_title_isunfreezed);
+		return array(
+			'msg'=>$_title_isunfreezed,
+			'body'=>str_replace('$1',htmlspecialchars(strip_bracket($vars['page'])),$_title_isunfreezed)
+		);
 	}
 	else if (md5($pass) == $adminpass)
 	{
@@ -37,29 +37,23 @@ function plugin_unfreeze_action()
 		
 		file_write(DATA_DIR,$vars['page'],$postdata,TRUE);
 		
-		$vars['cmd'] = 'read';
-		$msg = $_title_unfreezed;
-		$body = '';
-		
 		if (UNFREEZE_EDIT)
 		{
 			$vars['cmd'] = 'edit';
-			$body = edit_form($vars['page'],$postdata);
+			return array('msg'=>$_title_unfreezed,'body'=>'');
 		}
+		$vars['cmd'] = 'read';
+		return array(
+			'msg'=>$_title_unfreezed,
+			'body'=>edit_form($vars['page'],$postdata)
+		);
 	}
-	else
-	{
-		$msg = $_title_unfreeze;
-
-		$body = "<br />\n";
-		
-		if ($pass !== NULL)
-			$body .= "<strong>$_msg_invalidpass</strong><br />\n";
-		
-		$body.= "$_msg_unfreezing<br />\n";
-		
-		$s_page = htmlspecialchars($vars['page']);
-		$body .= <<<EOD
+	// 凍結解除フォームを表示
+	$s_page = htmlspecialchars($vars['page']);
+	
+	$body = ($pass === NULL) ? '' : "<p><strong>$_msg_invalidpass</strong></p>\n";
+	$body .= <<<EOD
+<p>$_msg_unfreezing</p>
 <form action="$script" method="post">
  <div>
   <input type="hidden" name="cmd" value="unfreeze" />
@@ -69,8 +63,7 @@ function plugin_unfreeze_action()
  </div>
 </form>
 EOD;
-	}
 	
-	return array('msg'=>$msg,'body'=>$body);
+	return array('msg'=>$_title_unfreeze,'body'=>$body);
 }
 ?>
