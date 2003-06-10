5b0b7414d3dfecac7894f1fca6f66b3b0fb7395a
arino <arino>
2003-06-10 23:02:16 +0900

    array_search()をin_array()に変更(FALSE/NULL問題)

--- plugin/ls.inc.php
+++ plugin/ls.inc.php
@@ -5,32 +5,39 @@
  * CopyRight 2002 Y.MASUI GPL2
  * http://masui.net/pukiwiki/ masui@masui.net
  *
- * $Id: ls.inc.php,v 1.7 2003/04/29 00:39:53 arino Exp $
+ * $Id: ls.inc.php,v 1.8 2003/06/10 14:02:16 arino Exp $
  */
 
 function plugin_ls_convert()
 {
 	global $vars;
 	
-	$aryargs = func_num_args() ? func_get_args() : array();
+	$with_title = FALSE;
 	
-	$with_title = (array_search('title',$aryargs)!==FALSE);
-	
-	$ls = array();
+	if (func_num_args())
+	{
+		$args = func_get_args();
+		$with_title = in_array('title',$args);
+	}
 	
 	$prefix = $vars['page'].'/';
 	
 	$pages = array();
-	foreach (get_existpages() as $page) {
-		if (strpos($page,$prefix) === 0) {
+	foreach (get_existpages() as $page)
+	{
+		if (strpos($page,$prefix) === 0)
+		{
 			$pages[] = $page;
 		}
 	}
 	natcasesort($pages);
 	
-	foreach ($pages as $page) {
+	$ls = array();
+	foreach ($pages as $page)
+	{
 		$comment = '';
-		if ($with_title) {
+		if ($with_title)
+		{
 			list($comment) = get_source($page);
 			// 見出しの固有ID部を削除
 			$comment = preg_replace('/^(\*{1,3}.*)\[#[A-Za-z][\w-]+\](.*)$/','$1$2',$comment);
