ba28ee7115dded6b7ccf271acbffac365de7b1b7
arino <arino>
2003-07-03 14:08:01 +0900

    BugTrack/393

---
+++ plugin/br.inc.php
@@ -0,0 +1,12 @@
+<?php
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: br.inc.php,v 1.1 2003/07/03 05:08:01 arino Exp $
+//
+
+function plugin_br_convert()
+{
+	return '<br class="spacer" />';
+}
+?>
