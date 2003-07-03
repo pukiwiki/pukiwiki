fa3c797eb3a4a58d2b8ee6c29f44961d37c3bb51
arino <arino>
2003-07-03 14:08:13 +0900

    BugTrack/394

---
+++ plugin/hr.inc.php
@@ -0,0 +1,12 @@
+<?php
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: hr.inc.php,v 1.1 2003/07/03 05:08:13 arino Exp $
+//
+
+function plugin_hr_convert()
+{
+	return '<hr class="short_line" />';
+}
+?>
