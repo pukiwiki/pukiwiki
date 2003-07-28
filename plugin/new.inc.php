58e51301a653ba15c872bec921f4bf78560ccb78
arino <arino>
2003-07-28 16:10:29 +0900

    -BugTrack/422: パラメータを拡張 -title属性にpassageを表示する

--- plugin/new.inc.php
+++ plugin/new.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: new.inc.php,v 1.2 2003/04/30 08:17:15 arino Exp $
+// $Id: new.inc.php,v 1.3 2003/07/28 07:10:29 arino Exp $
 //
 
 // 全体の表示フォーマット
@@ -15,36 +15,64 @@ function plugin_new_init()
 	// 経過秒数 => 新着表示タグ
 	$messages = array(
 		'_plugin_new_elapses' => array(
-			1*60*60*24 => ' <span class="new1">New!</span>',
-			5*60*60*24 => ' <span class="new5">New</span>',
+			1*60*60*24 => ' <span class="new1" title="%s">New!</span>',
+			5*60*60*24 => ' <span class="new5" title="%s">New</span>',
 		),
 	);
 	set_plugin_messages($messages);
 }
 function plugin_new_inline()
 {
-	global $_plugin_new_elapses;
+	global $vars,$_plugin_new_elapses;
 	
 	if (func_num_args() < 1)
 	{
 		return FALSE;
 	}
-	
+	$retval = '';
 	$args = func_get_args();
-	
-	$date = array_pop($args);
-	if (($timestamp = strtotime($date)) === -1)
+	$date = strip_htmltag(array_pop($args)); // {}部分の引数
+	if ($date != '' and ($timestamp = strtotime($date)) !== -1)
 	{
-		return FALSE;
+		$nodate = in_array('nodate',$args);
+		$timestamp -= ZONETIME;
+		$retval = $nodate ? '' : htmlspecialchars($date);
 	}
-	$retval = htmlspecialchars($date);
-
-	$erapse = UTIME - $timestamp + LOCALZONE;
+	else
+	{
+		$name = strip_bracket(count($args) ? array_shift($args) : $vars['page']);
+		$page = get_fullname($name,$vars['page']);   
+		$nolink = in_array('nolink',$args);
+		$timestamp = 0;
+		if (substr($page,-1) == '/')
+		{
+			foreach (preg_grep('/^'.preg_quote($page,'/').'/',get_existpages()) as $page)
+			{
+				$_timestamp = get_filetime($page);
+				if ($timestamp < $_timestamp)
+				{
+					$retval = $nolink ? '' : make_pagelink($page); // 最も新しいページを表示
+					$timestamp = $_timestamp;
+				}
+			}
+		}
+		else if (is_page($page))
+		{
+			$retval = $nolink ? '' : make_pagelink($page,$name);
+			$timestamp = get_filetime($page);
+		}
+		if ($timestamp == 0)
+		{
+			return '';
+		}
+	}
+	
+	$erapse = UTIME - $timestamp;
 	foreach ($_plugin_new_elapses as $limit=>$tag)
 	{
 		if ($erapse <= $limit)
 		{
-			$retval .= $tag;
+			$retval .= sprintf($tag,get_passage($timestamp));
 			break;
 		}
 	}
