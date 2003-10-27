a4fd71d6cac8c367d7a78bc16f0040c59a9b4358
arino <arino>
2003-10-27 11:45:52 +0900

    - BugTrack/476: $str_rules内で$vars['page']を参照できるように - &now;, &date;, &time;, &page;, &fpage;をデフォルトで有効に

--- pukiwiki.ini.php
+++ pukiwiki.ini.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: pukiwiki.ini.php,v 1.49 2003/10/27 02:43:19 arino Exp $
+// $Id: pukiwiki.ini.php,v 1.50 2003/10/27 02:45:52 arino Exp $
 //
 // PukiWiki setting file
 
@@ -275,48 +275,5 @@ $agents = array(
 	array('name'=>'i_mode','pattern'=>'#DoCoMo/(1\.0)/(?:[^/]+/c([0-9]+))?#'),
 	array('name'=>'i_mode','pattern'=>'#DoCoMo/(2\.0) [^(]+\(c([0-9]+)#'),
 	array('name'=>'i_mode','pattern'=>'#DDIPOCKET;JRC/[^/]+/(1\.0)/0100/c([0-9]+)#'),
-);
-
-/////////////////////////////////////////////////
-// ユーザ定義ルール
-//
-//  正規表現で記述してください。?(){}-*./+\$^|など
-//  は \? のようにクォートしてください。
-//  前後に必ず / を含めてください。行頭指定は ^ を頭に。
-//  行末指定は $ を後ろに。
-//
-/////////////////////////////////////////////////
-// ユーザ定義ルール(直接ソースを置換)
-$str_rules = array(
-	'now\?' => format_date(UTIME),
-	'date\?' => get_date($date_format),
-	'time\?' => get_date($time_format),
-//	'&now;' => format_date(UTIME),
-//	'&date;' => get_date($date_format),
-//	'&time;' => get_date($time_format),
-);
-
-/////////////////////////////////////////////////
-// フェイスマーク定義ルール(コンバート時に置換)
-// $usefacemark = 1ならフェイスマークが置換されます
-// 文章内にXDなどが入った場合にfacemarkに置換されてしまうので
-// 必要のない方は $usefacemarkを0にしてください。
-$facemark_rules = array(
-'\s(\:\))' => ' <img src="./face/smile.png" alt="$1" />',
-'\s(\:D)' => ' <img src="./face/bigsmile.png" alt="$1" />',
-'\s(\:p)' => ' <img src="./face/huh.png" alt="$1" />',
-'\s(\:d)' => ' <img src="./face/huh.png" alt="$1" />',
-'\s(XD)' => ' <img src="./face/oh.png" alt="$1" />',
-'\s(X\()' => ' <img src="./face/oh.png" alt="$1" />',
-'\s(;\))' => ' <img src="./face/wink.png" alt="$1" />',
-'\s(;\()' => ' <img src="./face/sad.png" alt="$1" />',
-'\s(\:\()' => ' <img src="./face/sad.png" alt="$1" />',
-'&amp;(smile);' => ' <img src="./face/smile.png" alt="$1" />',
-'&amp;(bigsmile);' => ' <img src="./face/bigsmile.png" alt="$1" />',
-'&amp;(huh);' => ' <img src="./face/huh.png" alt="$1" />',
-'&amp;(oh);' => ' <img src="./face/oh.png" alt="$1" />',
-'&amp;(wink);' => ' <img src="./face/wink.png" alt="$1" />',
-'&amp;(sad);' => ' <img src="./face/sad.png" alt="$1" />',
-'&amp;(heart);' => '<img src="./face/heart.png" alt="$1" />',
 );
 ?>
---
+++ rules.ini.php
@@ -0,0 +1,53 @@
+<?php
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: rules.ini.php,v 1.1 2003/10/27 02:45:52 arino Exp $
+//
+// PukiWiki setting file
+
+/////////////////////////////////////////////////
+// ユーザ定義ルール
+//
+//  正規表現で記述してください。?(){}-*./+\$^|など
+//  は \? のようにクォートしてください。
+//  前後に必ず / を含めてください。行頭指定は ^ を頭に。
+//  行末指定は $ を後ろに。
+//
+/////////////////////////////////////////////////
+// ユーザ定義ルール(直接ソースを置換)
+$str_rules = array(
+	'now\?' => format_date(UTIME),
+	'date\?' => get_date($date_format),
+	'time\?' => get_date($time_format),
+	'&now;' => format_date(UTIME),
+	'&date;' => get_date($date_format),
+	'&time;' => get_date($time_format),
+	'&page;' => array_pop(explode('/',$vars['page'])),
+	'&fpage;' => $vars['page'],
+);
+
+/////////////////////////////////////////////////
+// フェイスマーク定義ルール(コンバート時に置換)
+// $usefacemark = 1ならフェイスマークが置換されます
+// 文章内にXDなどが入った場合にfacemarkに置換されてしまうので
+// 必要のない方は $usefacemarkを0にしてください。
+$facemark_rules = array(
+'\s(\:\))' => ' <img src="./face/smile.png" alt="$1" />',
+'\s(\:D)' => ' <img src="./face/bigsmile.png" alt="$1" />',
+'\s(\:p)' => ' <img src="./face/huh.png" alt="$1" />',
+'\s(\:d)' => ' <img src="./face/huh.png" alt="$1" />',
+'\s(XD)' => ' <img src="./face/oh.png" alt="$1" />',
+'\s(X\()' => ' <img src="./face/oh.png" alt="$1" />',
+'\s(;\))' => ' <img src="./face/wink.png" alt="$1" />',
+'\s(;\()' => ' <img src="./face/sad.png" alt="$1" />',
+'\s(\:\()' => ' <img src="./face/sad.png" alt="$1" />',
+'&amp;(smile);' => ' <img src="./face/smile.png" alt="$1" />',
+'&amp;(bigsmile);' => ' <img src="./face/bigsmile.png" alt="$1" />',
+'&amp;(huh);' => ' <img src="./face/huh.png" alt="$1" />',
+'&amp;(oh);' => ' <img src="./face/oh.png" alt="$1" />',
+'&amp;(wink);' => ' <img src="./face/wink.png" alt="$1" />',
+'&amp;(sad);' => ' <img src="./face/sad.png" alt="$1" />',
+'&amp;(heart);' => '<img src="./face/heart.png" alt="$1" />',
+);
+?>
