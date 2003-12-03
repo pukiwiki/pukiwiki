59f7096e9163b2612b6b671cbe3e6b94a00ecb2a
arino <arino>
2003-12-03 21:29:19 +0900

    引数をチェック

--- plugin/md5.inc.php
+++ plugin/md5.inc.php
@@ -2,13 +2,17 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: md5.inc.php,v 1.2 2003/03/02 04:18:31 panda Exp $
+// $Id: md5.inc.php,v 1.3 2003/12/03 12:29:19 arino Exp $
 //
 //  MD5パスワードへの変換
 function plugin_md5_action()
 {
 	global $vars;
 	
+	if (!array_key_exists('md5',$vars))
+	{
+		return FALSE;
+	}
 	return array(
 		'msg'=>'Make password of MD5',
 		'body'=> htmlspecialchars($vars['md5']).' : '.md5($vars['md5'])
