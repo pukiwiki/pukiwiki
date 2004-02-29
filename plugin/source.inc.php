dd1b8ab7aafb03af4f1022939ffb68fe407ff0d7
arino <arino>
2004-02-29 16:05:26 +0900

    BugTrack/541 閲覧認証をチェック

--- plugin/source.inc.php
+++ plugin/source.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: source.inc.php,v 1.9 2003/07/03 05:25:53 arino Exp $
+// $Id: source.inc.php,v 1.10 2004/02/29 07:05:26 arino Exp $
 //
 // ページソースを表示
 
@@ -13,7 +13,7 @@ function plugin_source_action()
 	
 	$vars['refer'] = $vars['page'];
 	
-	if (!is_page($vars['page']))
+	if (!is_page($vars['page']) || !check_readable($vars['page'],false,false)))
 	{
 		return array(
 			'msg'=>$_source_messages['msg_notfound'],
