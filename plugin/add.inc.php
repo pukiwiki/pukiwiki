3d5c3682d69dccff1fbadb6c40e8b0812fd9efd2
henoheno <henoheno>
2004-07-01 23:53:59 +0900

    Please use abstruct method $vars, instead of $get or $post

--- plugin/add.inc.php
+++ plugin/add.inc.php
@@ -2,22 +2,24 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: add.inc.php,v 1.2 2003/05/14 10:08:48 arino Exp $
+// $Id: add.inc.php,v 1.3 2004/07/01 14:53:59 henoheno Exp $
 //
 // ÄÉ²Ã
 // cmd=add
 function plugin_add_action()
 {
-	global $post,$vars,$_title_add,$_msg_add;
+	global $get,$post,$vars,$_title_add,$_msg_add;
+
+	$page = isset($vars['page']) ? $vars['page'] : '';
+
+	check_editable($page);
 	
-	check_editable($vars['page']);
-	
-	$vars['add'] = $post['add'] = TRUE;
+	$get['add'] = $post['add'] = $vars['add'] = TRUE;
 	
 	return array(
 		'msg' => $_title_add,
 		'body' => "<ul>\n <li>$_msg_add</li>\n</ul>" .
-			edit_form($vars['page'],'')
+			edit_form($page, '')
 	);
 }
 ?>
