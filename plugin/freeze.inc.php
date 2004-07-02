a3e2db7a8800e447d4b3259909d5beb8669f3f5b
henoheno <henoheno>
2004-07-02 22:59:58 +0900

    Please use abstruct method $vars, instead of $get or $post

--- plugin/freeze.inc.php
+++ plugin/freeze.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: freeze.inc.php,v 1.4 2003/06/05 10:38:03 arino Exp $
+// $Id: freeze.inc.php,v 1.5 2004/07/02 13:59:58 henoheno Exp $
 //
 // 凍結
 function plugin_freeze_convert()
@@ -12,51 +12,47 @@ function plugin_freeze_convert()
 
 function plugin_freeze_action()
 {
-	global $script,$post,$vars,$function_freeze,$adminpass;
-	global $_title_isfreezed,$_title_freezed,$_title_freeze;
-	global $_msg_invalidpass,$_msg_freezing,$_btn_freeze;
+	global $script, $vars, $function_freeze, $adminpass;
+	global $_title_isfreezed, $_title_freezed, $_title_freeze;
+	global $_msg_invalidpass, $_msg_freezing, $_btn_freeze;
+
+	$page = isset($vars['page']) ? $vars['page'] : '';
+
+	if (!$function_freeze or !is_page($page))
+		return array('msg' => '', 'body' => '');
 	
-	if (!$function_freeze or !is_page($vars['page']))
-	{
-		return array('msg'=>'','body'=>'');
-	}
-	
-	$pass = array_key_exists('pass',$post) ? $post['pass'] : NULL;
-	
-	if (is_freeze($vars['page']))
-	{
+	$pass = isset($vars['pass']) ? $vars['pass'] : NULL;
+	if (is_freeze($page)) {
 		return array(
-			'msg' => $_title_isfreezed,
-			'body' => str_replace('$1',htmlspecialchars(strip_bracket($vars['page'])),$_title_isfreezed)
+			'msg'  => $_title_isfreezed,
+			'body' => str_replace('$1', htmlspecialchars(strip_bracket($page)), $_title_isfreezed)
 		);
-	}
-	else if (md5($pass) == $adminpass)
-	{
-		$postdata = get_source($post['page']);
-		array_unshift($postdata,"#freeze\n");
-		$postdata = join('',$postdata);
+	} else if (md5($pass) == $adminpass) {
+		$postdata = get_source($page);
+		array_unshift($postdata, "#freeze\n");
+		$postdata = join('', $postdata);
 		
-		file_write(DATA_DIR,$vars['page'],$postdata,TRUE);
+		file_write(DATA_DIR,$page, $postdata, TRUE);
 		
 		$vars['cmd'] = 'read';
-		return array('msg'=>$_title_freezed,'body'=>'');
+		return array('msg' => $_title_freezed, 'body' => '');
 	}
 	// 凍結フォームを表示
-	$s_page = htmlspecialchars($vars['page']);
+	$s_page = htmlspecialchars($page);
 	
 	$body = ($pass === NULL) ? '' : "<p><strong>$_msg_invalidpass</strong></p>\n";
 	$body .= <<<EOD
 <p>$_msg_freezing</p>
 <form action="$script" method="post">
  <div>
-  <input type="hidden" name="cmd" value="freeze" />
-  <input type="hidden" name="page" value="$s_page" />
+  <input type="hidden"   name="cmd"  value="freeze" />
+  <input type="hidden"   name="page" value="$s_page" />
   <input type="password" name="pass" size="12" />
-  <input type="submit" name="ok" value="$_btn_freeze" />
+  <input type="submit"   name="ok"   value="$_btn_freeze" />
  </div>
 </form>
 EOD;
 	
-	return array('msg'=>$_title_freeze,'body'=>$body);
+	return array('msg'=>$_title_freeze, 'body'=>$body);
 }
 ?>
--- plugin/unfreeze.inc.php
+++ plugin/unfreeze.inc.php
@@ -2,68 +2,66 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: unfreeze.inc.php,v 1.4 2003/06/05 10:38:03 arino Exp $
+// $Id: unfreeze.inc.php,v 1.5 2004/07/02 13:59:58 henoheno Exp $
 //
 // 凍結解除
 
 // 凍結解除時にページの編集フォームを表示するか
-define('UNFREEZE_EDIT',FALSE);
+define('UNFREEZE_EDIT', FALSE);
 
 function plugin_unfreeze_action()
 {
-	global $script,$post,$vars,$function_freeze,$adminpass;
-	global $_title_isunfreezed,$_title_unfreezed,$_title_unfreeze;
-	global $_msg_invalidpass,$_msg_unfreezing,$_btn_unfreeze;
+	global $script, $vars, $function_freeze, $adminpass;
+	global $_title_isunfreezed, $_title_unfreezed, $_title_unfreeze;
+	global $_msg_invalidpass, $_msg_unfreezing, $_btn_unfreeze;
+
+	$page = isset($vars['page']) ? $vars['page'] : '';
+
+	if (!$function_freeze or !is_page($page))
+		return array('msg' => '', 'body' => '');
 	
-	if (!$function_freeze or !is_page($vars['page']))
-	{
-		return array('msg'=>'','body'=>'');
-	}
+	$pass = isset($vars['pass']) ? $vars['pass'] : NULL;
 	
-	$pass = array_key_exists('pass',$post) ? $post['pass'] : NULL;
-	
-	if (!is_freeze($vars['page']))
-	{
+	if (!is_freeze($page)) {
 		return array(
-			'msg'=>$_title_isunfreezed,
-			'body'=>str_replace('$1',htmlspecialchars(strip_bracket($vars['page'])),$_title_isunfreezed)
+			'msg'  => $_title_isunfreezed,
+			'body' => str_replace('$1', htmlspecialchars(strip_bracket($page)), $_title_isunfreezed)
 		);
-	}
-	else if (md5($pass) == $adminpass)
-	{
-		$postdata = get_source($post['page']);
+	} else if (md5($pass) == $adminpass) {
+		$postdata = get_source($page);
 		array_shift($postdata);
-		$postdata = join('',$postdata);
+		$postdata = join('', $postdata);
 		
-		file_write(DATA_DIR,$vars['page'],$postdata,TRUE);
+		file_write(DATA_DIR, $page, $postdata, TRUE);
 		
-		if (UNFREEZE_EDIT)
-		{
+		if (UNFREEZE_EDIT) {
 			$vars['cmd'] = 'edit';
-			return array('msg'=>$_title_unfreezed,'body'=>'');
+			return array('msg' => $_title_unfreezed, 'body' => '');
+		} else {
+			$vars['cmd'] = 'read';
+			return array(
+				'msg'  => $_title_unfreezed,
+				'body' => edit_form($page, $postdata)
+			);
 		}
-		$vars['cmd'] = 'read';
-		return array(
-			'msg'=>$_title_unfreezed,
-			'body'=>edit_form($vars['page'],$postdata)
-		);
 	}
+
 	// 凍結解除フォームを表示
-	$s_page = htmlspecialchars($vars['page']);
+	$s_page = htmlspecialchars($page);
 	
 	$body = ($pass === NULL) ? '' : "<p><strong>$_msg_invalidpass</strong></p>\n";
 	$body .= <<<EOD
 <p>$_msg_unfreezing</p>
 <form action="$script" method="post">
  <div>
-  <input type="hidden" name="cmd" value="unfreeze" />
-  <input type="hidden" name="page" value="$s_page" />
+  <input type="hidden"   name="cmd"  value="unfreeze" />
+  <input type="hidden"   name="page" value="$s_page" />
   <input type="password" name="pass" size="12" />
-  <input type="submit" name="ok" value="$_btn_unfreeze" />
+  <input type="submit"   name="ok"   value="$_btn_unfreeze" />
  </div>
 </form>
 EOD;
 	
-	return array('msg'=>$_title_unfreeze,'body'=>$body);
+	return array('msg' => $_title_unfreeze, 'body' => $body);
 }
 ?>
