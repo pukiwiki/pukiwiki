b7965593ea4e0b536bdd4c5f6b0fd379d9e4bdfe
henoheno <henoheno>
2004-07-17 19:16:47 +0900

    Rewrote APOP/POP login function. APOP now enabled, and will use APOP if possible, by default

--- mail.php
+++ mail.php
@@ -2,80 +2,66 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: mail.php,v 1.1 2003/09/24 00:37:27 arino Exp $
+// $Id: mail.php,v 1.2 2004/07/17 10:16:47 henoheno Exp $
 //
 
-// POP Before SMTP
-function pop_before_smtp()
+// APOP/POP Before SMTP
+function pop_before_smtp($pop_userid = '', $pop_passwd = '',
+	$pop_server = 'localhost', $pop_port = 110)
 {
-	global $pop_auth_use_apop,$pop_port,$pop_server,$pop_userid,$pop_passwd;
+	// Always try APOP, by default
+	$pop_auth_use_apop =
+		(! isset($GLOBALS['pop_auth_use_apop']) || $GLOBALS['pop_auth_use_apop'] !== FALSE);
 
-	$fp = @fsockopen($pop_server,$pop_port,$errno,$errstr,30);
-	if (!$fp)
-	{
-		return "$errstr ($errno)";
-	}
-	
-	$rc = pop_result($fp);
-	if ($rc !== 0)
-	{
-		return $rc;
-	}
-	
-	// APOP 指定時の利用判定
-	if ($pop_auth_use_apop)
-	{
-		// digest 抽出
-		preg_match("(<.*>)", $buf, $regs);
-		// digest が抽出できなかった場合は、強制的に POP に切り替え
-		if (empty($regs[0]))
-		{
-			$pop_auth_use_apop = 0;
-		}
-	}
-	
-	// APOP または POP
-	if ($pop_auth_use_apop)
-	{
-		fputs($fp, "APOP ".$pop_userid." ".md5($regs[0].$pop_passwd)."\r\n");
-		$rc = pop_result($fp);
-		if ($rc !== 0)
-		{
-			return $rc;
-		}
-	}
-	else
-	{
-		fputs($fp, "USER ".$pop_userid."\r\n");
-		$rc = pop_result($fp);
-		if ($rc !== 0)
-		{
-			return $rc;
-		}
-		
-		fputs($fp, "PASS ".$pop_passwd."\r\n");
-		$rc = pop_result($fp);
-		if ($rc !== 0)
-		{
-			return $rc;
-		}
+	// Compat: GLOBALS > function arguments
+	foreach(array('pop_userid', 'pop_passwd', 'pop_server', 'pop_port') as $global) {
+		if(isset($GLOBALS[$global]) && $GLOBALS[$global] !== '')
+			${$global} = $GLOBALS[$global];
 	}
 
-	fputs($fp, "QUIT\r\n");
+	// Check
+	$die = '';
+	foreach(array('pop_userid', 'pop_server', 'pop_port') as $global)
+		if(${$global} == '') $die .= "pop_before_smtp(): \$$global seems blank\n";
+	if ($die) return ($die);
+
+	// Connect
+	$errno = 0; $errstr = '';
+	$fp = @fsockopen($pop_server, $pop_port, $errno, $errstr, 30);
+	if (! $fp) return ("pop_before_smtp(): $errstr ($errno)");
+
+	// Greeting message from server, may include <challenge-string> of APOP
+	$message = fgets($fp, 1024); // 512byte max
+	if (! preg_match('/^\+OK /', $message)) {
+		fclose($fp);
+		return ("pop_before_smtp(): Greeting message seems invalid");
+	}
+	$challenge = array();
+	if ($pop_auth_use_apop && preg_match('/<.*>/', $message, $challenge)) {
+		// APOP auth
+		$response = md5($challenge[0] . $pop_passwd);
+		fputs($fp, 'APOP ' . $pop_userid . ' ' . $response . "\r\n");
+	} else {
+		// POP auth
+		fputs($fp, 'USER ' . $pop_userid . "\r\n");
+		$message = fgets($fp, 1024); // 512byte max
+		if (! preg_match('/^\+OK /', $message)) {
+			fclose($fp);
+			return ("pop_before_smtp(): USER seems invalid");
+		}
+		fputs($fp, 'PASS ' . $pop_passwd . "\r\n");
+	}
+	$result = fgets($fp, 1024); // 512byte max, auth result
+
+	// Disconnect
+	// fputs($fp, "QUIT\r\n");
+	// $message = fgets($fp, 1024); // 512byte max, last "+OK"
 	fclose($fp);
-	return 0;
-}
 
-// POP 処理の結果判定
-function pop_result($fp)
-{
-	$buf = fgets($fp, 2048);
-	$tok = strtok($buf,' ');
-	if ($tok == '+OK')
-	{
-		return 0;
+	if (! preg_match('/^\+OK /', $result)) {
+		return ("pop_before_smtp(): Authentication failed");
+	} else {
+		return TRUE;	// Success
 	}
-	fclose($fp);
-	return substr($buf, strlen($tok) + 1);
 }
 ?>
