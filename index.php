92ea2b4d3d4ae96395989b1822c3ae24c71361ea
henoheno <henoheno>
2004-07-24 21:55:18 +0900

    Added index.php, encourages data-and-code concealment from direct HTTP access

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: index.php,v 1.1 2004/07/24 12:55:18 henoheno Exp $
+
+// Directory definition
+// (Ended with a slash like '../path/to/pkwk/', or '')
+define('DATA_HOME',	'');
+define('SUB_DIR',	'');
+
+require(SUB_DIR . 'pukiwiki.php');
+?>
