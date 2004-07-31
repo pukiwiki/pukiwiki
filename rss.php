fa128d6de3b48dc59eb3b7aa9ced64b170cbdf0f
henoheno <henoheno>
2004-07-31 20:32:38 +0900

    Cleanup, and correct $item => $items

--- rss.php
+++ rss.php
@@ -1,74 +1,65 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: rss.php,v 1.5.2.3 2004/07/31 03:15:07 henoheno Exp $
+// $Id: rss.php,v 1.5.2.4 2004/07/31 11:32:38 henoheno Exp $
 /////////////////////////////////////////////////
 
 // RecentChanges の RSS を出力
 function catrss($rss)
 {
-	global $rss_max,$page_title,$WikiName,$BracketName,$script,$whatsnew;
+	global $rss_max, $page_title, $WikiName, $BracketName, $script, $whatsnew;
 
 	$lines = get_source($whatsnew);
-	header("Content-type: application/xml");
+	header('Content-type: application/xml');
 
 	$self = (preg_match('#^https?://#',$script) ? $script : get_script_uri());
 	if ($self === FALSE)
-	{
-		die_message("please set '\$script' in ".INI_FILE);
-	}
+		die_message("Please set '\$script' in " . INI_FILE);
 
 	$page_title_utf8 = $page_title;
-	if(function_exists("mb_convert_encoding"))
-		$page_title_utf8 = mb_convert_encoding($page_title_utf8,"UTF-8","auto");
+	if(function_exists('mb_convert_encoding'))
+		$page_title_utf8 = mb_convert_encoding($page_title_utf8, 'UTF-8', 'auto');
 
-	$item = "";
-	$rdf_li = "";
+	$items = $rdf_li = '';
 	$cnt = 0;
+	$match = array();
 	foreach($lines as $line)
 	{
 		if($cnt > $rss_max - 1) break;
 
-		if(preg_match("/(($WikiName)|($BracketName))/",$line,$match))
+		if(preg_match("/(($WikiName)|($BracketName))/", $line, $match))
 		{
-			if($match[2])
-			{
-				$title = $url = $match[1];
+			if($match[2]) {
+				$title = $match[1]; // WikiName
+			} else {
+				$title = strip_bracket($match[1]); // BracketName
+				if(function_exists('mb_convert_encoding'))
+					$title = mb_convert_encoding($title, 'UTF-8', 'auto');
 			}
-			else
-			{
-				if(function_exists("mb_convert_encoding"))
-					$title = mb_convert_encoding(strip_bracket($match[1]),"UTF-8","auto");
-				else
-					$title = strip_bracket($match[1]);
-
-				$url = $match[1];
-			}
-
+			$url = $match[1];
 			$title = htmlspecialchars($title);
 
-			$dcdate = substr_replace(date("Y-m-d\TH:i:sO"),':',-2,0);
-			$desc = date("D, d M Y H:i:s T",filemtime(get_filename(encode($match[1]))));
+			$dcdate = substr_replace(date("Y-m-d\TH:i:sO"), ':', -2, 0);
+			$desc = date("D, d M Y H:i:s T", filemtime(get_filename(encode($match[1]))));
 
-			if($rss==2)
-				$items.= "<item rdf:about=\"$self?".rawurlencode($url)."\">\n";
-			else
-				$items.= "<item>\n";
-			$items.= " <title>$title</title>\n";
-			$items.= " <link>$self?".rawurlencode($url)."</link>\n";
-			if($rss==2)
-			{
+			if($rss==2) {
+				$items .= "<item rdf:about=\"$self?" . rawurlencode($url) . "\">\n";
+			} else {
+				$items .= "<item>\n";
+			}
+			$items .= " <title>$title</title>\n";
+			$items .= " <link>$self?".rawurlencode($url)."</link>\n";
+			if($rss == 2) {
 				$items.= " <dc:date>$dcdate</dc:date>\n";
 			}
-			$items.= " <description>$desc</description>\n";
-			$items.= "</item>\n\n";
-			$rdf_li.= "    <rdf:li rdf:resource=\"$self?".rawurlencode($url)."\" />\n";
+			$items .= " <description>$desc</description>\n";
+			$items .= "</item>\n\n";
+			$rdf_li .= "    <rdf:li rdf:resource=\"$self?" . rawurlencode($url) . "\" />\n";
 
 		}
-
-		$cnt++;
+		++$cnt;
 	}
 
-	if($rss==1)
+	if($rss == 1)
 	{
 ?>
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
@@ -90,7 +81,7 @@ function catrss($rss)
 </rss>
 <?php
 	}
-	else if($rss==2)
+	else if($rss == 2)
 	{
 ?>
 <?php echo '<?xml version="1.0" encoding="utf-8"?>' ?>
