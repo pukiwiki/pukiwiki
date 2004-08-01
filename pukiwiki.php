f9ae046d7a9348dbc5c1627ac4c7265d184954f6
henoheno <henoheno>
2004-08-01 10:58:09 +0900

    Copy of index.php

---
+++ pukiwiki.php
@@ -0,0 +1,11 @@
+<?php
+// PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: pukiwiki.php,v 1.38 2004/08/01 01:58:09 henoheno Exp $
+
+// Directory definition
+// (Ended with a slash like '../path/to/pkwk/', or '')
+define('DATA_HOME',	'');
+define('LIB_DIR',	'lib/');
+
+require(LIB_DIR . 'pukiwiki.php');
+?>
