147f111ee5b2ab654f2053e17ee6cd82b70ffd8e
henoheno <henoheno>
2004-08-06 21:29:31 +0900

    * Cleanup, shrink * Replace image-tag with any case (IMG or img or ...) * Replace image-tag without alt option, if exists * Cut 5 bytes of newline

--- skin/keitai.skin.ja.php
+++ skin/keitai.skin.ja.php
@@ -2,35 +2,38 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: keitai.skin.ja.php,v 1.5 2004/04/03 15:56:08 arino Exp $
+// $Id: keitai.skin.ja.php,v 1.6 2004/08/06 12:29:31 henoheno Exp $
 //
 
-if (!defined('DATA_DIR')) { exit; }
+global $script, $vars, $page_title, $max_size, $accesskey;
 
-global $script,$vars,$page_title,$max_size,$accesskey;
+if (! defined('DATA_DIR')) exit;
 
+// Shift JIS encode
 header('Content-Type: text/html; charset=Shift_JIS');
+$title = mb_convert_encoding($title, 'SJIS', SOURCE_ENCODING);
+$body  = mb_convert_encoding($body,  'SJIS', SOURCE_ENCODING);
 
 //1KByte余裕を見る(ヘッダなど)
 $max_size = (--$max_size * 1024);
 
-// エンコード
-$title = mb_convert_encoding($title,'SJIS',SOURCE_ENCODING);
-$body = mb_convert_encoding($body,'SJIS',SOURCE_ENCODING);
+// ALT option を持つ IMG タグ(画像)を文字列に置換
+$body = preg_replace('#(<div[^>]+>)?(<a[^>]+>)?<img[^>]*alt="([^"]+)"[^>]*>(?(2)</a>)(?(1)</div>)#i', '$3', $body);
 
-// 画像を置換
-$body = preg_replace('#(<div[^>]+>)?(<a[^>]+>)?<img[^>]*alt="([^"]+)"[^>]*>(?(2)</a>)(?(1)</div>)#','$3',$body);
+// ALT option の無い IMG タグ(画像)を文字列に置換
+$body = preg_replace('#(<div[^>]+>)?(<a[^>]+>)?<img[^>]+>(?(2)</a>)(?(1)</div>)#i', '[img]', $body);
 
 // ページ番号
+$r_page = isset($vars['page'] ? $vars['page'] : '';
+$r_page = rawurlencode($r_page);
 $pageno = (isset($vars['p']) and is_numeric($vars['p'])) ? $vars['p'] : 0;
 $pagecount = ceil(strlen($body) / $max_size);
 $lastpage = $pagecount - 1;
-$r_page = rawurlencode($vars['page']);
 
 // ナビゲーション
 $navi = array();
 $navi[] = "<a href=\"$link_top\" $accesskey=\"0\">0.Top</a>";
-$navi[] = "<a href=\"$script?plugin=newpage&amp;refer=$r_page\" $accesskey=\"1\">1.New</a>";
+$navi[] = "<a href=\"$script?plugin=newpage&refer=$r_page\" $accesskey=\"1\">1.New</a>";
 $navi[] = "<a href=\"$link_edit\" $accesskey=\"2\">2.Edit</a>";
 if ($is_read and $function_freeze) {
 	if ($is_freeze) {
@@ -48,19 +51,22 @@ if ($pagecount > 1) {
 	$prev = $pageno - 1;
 	$next = $pageno + 1;
 	if ($pageno > 0) {
-		$navi[] = "<a href=\"$script?cmd=read&amp;page=$r_page&amp;p=$prev\" $accesskey=\"7\">7.Prev</a>";
+		$navi[] = "<a href=\"$script?cmd=read&page=$r_page&p=$prev\" $accesskey=\"7\">7.Prev</a>";
 	}
 	$navi[] = "$next/$pagecount ";
 	if ($pageno < $lastpage) {
-		$navi[] = "<a href=\"$script?cmd=read&amp;page=$r_page&amp;p=$next\" $accesskey=\"8\">8.Next</a>";
+		$navi[] = "<a href=\"$script?cmd=read&page=$r_page&p=$next\" $accesskey=\"8\">8.Next</a>";
 	}
 }
-$navi = join(' | ',$navi);
-$body = substr($body,$pageno * $max_size, $max_size);
-?>
-<html>
-<head><title><?php echo $title ?></title></head>
-<body>
-<?php echo $navi ?><hr><?php echo $body ?>
-</body>
-</html>
+
+$navi = join(' | ', $navi);
+$body = substr($body, $pageno * $max_size, $max_size);
+
+// Output
+?><html><head><title><?php
+	echo $title
+?></title></head><body><?php
+	echo $navi
+?><hr><?php
+	echo $body
+?></body></html>
