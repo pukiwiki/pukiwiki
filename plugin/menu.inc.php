eb8c6b14fdf03dbed6b29d4021cb38ba813d78fd
henoheno <henoheno>
2004-08-06 22:10:39 +0900

    Just a cleanup

--- plugin/menu.inc.php
+++ plugin/menu.inc.php
@@ -2,46 +2,41 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: menu.inc.php,v 1.3 2004/07/31 03:09:20 henoheno Exp $
+// $Id: menu.inc.php,v 1.4 2004/08/06 13:10:39 henoheno Exp $
 //
 
 // サブメニューを使用する
-define('MENU_ENABLE_SUBMENU',FALSE);
+define('MENU_ENABLE_SUBMENU', FALSE);
 
 function plugin_menu_convert()
 {
-	global $script,$vars,$menubar;
+	global $vars, $menubar;
 	static $menu = NULL;
 
-	if (func_num_args())
-	{
+	if (func_num_args()) {
 		$args = func_get_args();
-		if (is_page($args[0]))
-		{
-			$menu = $args[0];
-		}
+		if (is_page($args[0])) $menu = $args[0];
 		return '';
 	}
 
 	$page = ($menu === NULL) ? $menubar : $menu;
-	if (MENU_ENABLE_SUBMENU)
-	{
-		$path = explode('/',strip_bracket($vars['page']));
-		while(count($path))
-		{
-			$_page = join('/',$path).'/MenuBar';
-			if (is_page($_page))
-			{
+
+	if (MENU_ENABLE_SUBMENU) {
+		$path = explode('/', strip_bracket($vars['page']));
+		while(count($path)) {
+			$_page = join('/', $path) . '/MenuBar';
+			if (is_page($_page)) {
 				$page = $_page;
 				break;
 			}
 			array_pop($path);
 		}
 	}
-	if (!is_page($page))
-	{
+
+	if (! is_page($page)) {
 		return '';
+	} else {
+		return preg_replace('/<ul[^>]*>/', '<ul>', convert_html(get_source($page)));
 	}
-	return preg_replace('/<ul[^>]*>/','<ul>',convert_html(get_source($page)));
 }
 ?>
