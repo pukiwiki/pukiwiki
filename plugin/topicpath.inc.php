af29d8518c40bb432e3bf4398c87f94280e8c380
henoheno <henoheno>
2004-08-12 21:50:01 +0900

    BugTrack/653 Correct when THIS_PAGE_DISPLAY = FALSE and THIS_PAGE_LINK = FALSE. Cleanup. Simplify. Added spaces.

--- plugin/topicpath.inc.php
+++ plugin/topicpath.inc.php
@@ -2,54 +2,63 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: topicpath.inc.php,v 1.2 2004/08/04 11:21:13 henoheno Exp $
-//
+// $Id: topicpath.inc.php,v 1.3 2004/08/12 12:50:01 henoheno Exp $
 // topicpath plugin for PukiWiki
 //   available under the GPL
 
+// $defaultpageへのリンクも表示するかどうか
+// TRUE:表示する FALSE:表示しない.
+define('PLUGIN_TOPICPATH_TOP_DISPLAY', TRUE);
+// $defaultpageに対するラベル
+define('PLUGIN_TOPICPATH_TOP_LABEL', 'Top');
 
-//	defaultpageを一番最初に表示するかどうか。TRUE:表示する FALSE:表示しない.
-define('PLUGIN_TOPICPATH_TOP_DISPLAY',TRUE);
-//	$defaultpageに対するラベル
-define('PLUGIN_TOPICPATH_TOP_LABEL','Top');
-//	階層を区切るセパレータ
-define('PLUGIN_TOPICPATH_TOP_SEPARATOR',' / ');
-//	自分のページに対するリンクを表示するかどうか
-define('PLUGIN_TOPICPATH_THIS_PAGE_DISPLAY',TRUE);
-//	自分のページに対してリンクするかどうか
-define('PLUGIN_TOPICPATH_THIS_PAGE_LINK',TRUE);
+// 階層を区切るセパレータ
+define('PLUGIN_TOPICPATH_TOP_SEPARATOR', ' / ');
+
+// そのページ自身を表示するか
+define('PLUGIN_TOPICPATH_THIS_PAGE_DISPLAY', TRUE);
+// 表示する場合、自分自身を指すリンクを表示するか
+define('PLUGIN_TOPICPATH_THIS_PAGE_LINK', TRUE);
 
 function plugin_topicpath_convert()
 {
-	return '<div>'.plugin_topicpath_inline().'</div>';
+	return '<div>' . plugin_topicpath_inline() . '</div>';
 }
 
 function plugin_topicpath_inline()
 {
-	global $script,$vars,$defaultpage;
-	
-	$args = func_get_args();
-	
-	$page = $vars['page'];
-	
-	if ($page == $defaultpage) { return ''; }
-	
-	$topic_path = array();
+	global $script, $vars, $defaultpage;
+
+	// $args = func_get_args();
+
+	$page = isset($vars['page']) ? $vars['page'] : '';
+	if ($page == '' || $page == $defaultpage) return '';
+
 	$parts = explode('/', $page);
 
-	if (!PLUGIN_TOPICPATH_THIS_PAGE_DISPLAY) { array_pop($parts); }
+	if (PLUGIN_TOPICPATH_THIS_PAGE_DISPLAY) {
+		$b_link = PLUGIN_TOPICPATH_THIS_PAGE_LINK;
+	} else {
+		array_pop($parts); // Remove itself
+		$b_link = TRUE;    // Link to the parent
+	}
 
-	$b_link = PLUGIN_TOPICPATH_THIS_PAGE_LINK;
-	while (count($parts)) {
-		$landing = join('/', $parts);
-		$element = array_pop($parts);
-		$topic_path[] = $b_link ? "<a href=\"$script?".rawurlencode($landing)."\">$element</a>" : htmlspecialchars($element);
-		$b_link = TRUE;
+	$topic_path = array();
+	while (! empty($parts)) {
+		$landing = rawurlencode(join('/', $parts));
+		$element = htmlspecialchars(array_pop($parts));
+		if ($b_link)  {
+			$topic_path[] = "<a href=\"$script?$landing\">$element</a>";
+		} else {
+			$topic_path[] = $element;
+			$b_link = TRUE; // Maybe reacheable once at a time
+		}
 	}
-	if (PLUGIN_TOPICPATH_TOP_DISPLAY)
-	{
-		$topic_path[] = make_pagelink($defaultpage,PLUGIN_TOPICPATH_TOP_LABEL);
+
+	if (PLUGIN_TOPICPATH_TOP_DISPLAY) {
+		$topic_path[] = make_pagelink($defaultpage, PLUGIN_TOPICPATH_TOP_LABEL);
 	}
+
 	return join(PLUGIN_TOPICPATH_TOP_SEPARATOR, array_reverse($topic_path));
 }
 ?>
