1f2b49ca06a65d460b52abd4fab29c33099946d3
henoheno <henoheno>
2004-08-23 20:46:10 +0900

    Just added a comment

--- plugin/clear.inc.php
+++ plugin/clear.inc.php
@@ -2,9 +2,9 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: clear.inc.php,v 1.2 2004/08/04 11:21:13 henoheno Exp $
+// $Id: clear.inc.php,v 1.3 2004/08/23 11:46:10 henoheno Exp $
 //
-// div class="clear"を表示する
+// div class="clear"を表示し、テキストの回りこみを解除する
 // plugin=clear
 
 function plugin_clear_convert() {
