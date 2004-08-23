d9f7c5c385f7899ef232e7b4d6a7101f2da54adb
henoheno <henoheno>
2004-08-23 20:41:57 +0900

    BugTrack/664 Check the 2nd argument first, for compatibility

--- plugin/img.inc.php
+++ plugin/img.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: img.inc.php,v 1.11 2004/08/19 11:44:03 henoheno Exp $
+// $Id: img.inc.php,v 1.12 2004/08/23 11:41:57 henoheno Exp $
 //
 
 // Stop word-wrapping
@@ -13,11 +13,8 @@ function plugin_img_convert()
 {
 	$usage = "#img(): Usage: (URI-to-image[,right[,clear]])<br />\n";
 	$args = func_get_args();
-	$url = isset($args[0]) ? $args[0] : '';
 
-	if (! is_url($url) || ! preg_match('/\.(jpe?g|gif|png)$/i', $url))
-		return $usage;
-
+	// Check the 2nd argument first, for compatibility
 	$arg = isset($args[1]) ? strtoupper($args[1]) : '';
 	if ($arg == '' || $arg == 'L' || $arg == 'LEFT') {
 		$align = 'left';
@@ -25,8 +22,13 @@ function plugin_img_convert()
 		$align = 'right';
 	} else {
 		// Stop word-wrapping only (Ugly but compatible)
+		// Short usage: #img(,clear)
 		return PLUGIN_IMG_CLEAR;
 	}
+
+	$url = isset($args[0]) ? $args[0] : '';
+	if (! is_url($url) || ! preg_match('/\.(jpe?g|gif|png)$/i', $url))
+		return $usage;
 
 	$arg = isset($args[2]) ? strtoupper($args[2]) : '';
 	$clear = ($arg == 'C' || $arg == 'CLEAR') ? PLUGIN_IMG_CLEAR : '';
