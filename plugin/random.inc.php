bd0529084a22269ed091a25bb532c678f25eaa14
henoheno <henoheno>
2004-09-10 23:18:34 +0900

    Simplify. Shrink. Remove unused global. Add a rawurlencode(). Correct default text as a random link

--- plugin/random.inc.php
+++ plugin/random.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: random.inc.php,v 1.7 2004/08/06 16:13:18 henoheno Exp $
+// $Id: random.inc.php,v 1.8 2004/09/10 14:18:34 henoheno Exp $
 //
 
 /*
@@ -17,42 +17,38 @@
  リンクに表示する文字列
 
  */
+
 function plugin_random_convert()
 {
 	global $script, $vars;
 
-	$title = 'press here.';
-
-	if (func_num_args())
-	{
-		$args = func_get_args();
-		$title = htmlspecialchars($args[0]);
+	$title = '[Random Link]'; // default
+	if (func_num_args()) {
+		$args  = func_get_args();
+		$title = $args[0];
 	}
-	return "<p><a href=\"$script?plugin=random&amp;refer={$vars['page']}\">$title</a></p>";
+
+	return "<p><a href=\"$script?plugin=random&amp;refer=" .
+		rawurlencode($vars['page']) . '">' .
+		htmlspecialchars($title) . '</a></p>';
 }
 
 function plugin_random_action()
 {
-	global $script, $vars;
+	global $vars;
 
 	$pattern = strip_bracket($vars['refer']) . '/';
-
 	$pages = array();
-	foreach (get_existpages() as $_page)
-	{
-		if (strpos($_page,$pattern) === 0)
-		{
+	foreach (get_existpages() as $_page) {
+		if (strpos($_page, $pattern) === 0)
 			$pages[$_page] = strip_bracket($_page);
-		}
 	}
-//	natcasesort($pages);
-	srand((double)microtime()*1000000);
+
+	srand((double)microtime() * 1000000);
 	$page = array_rand($pages);
 
-	if ($page != '')
-	{
-		$vars['refer'] = $page;
-	}
+	if ($page != '') $vars['refer'] = $page;
+
 	return array('body'=>'','msg'=>'');
 }
 ?>
