674ab0031fb525f8b937962dc97034b929738776
henoheno <henoheno>
2004-10-07 22:02:48 +0900

    Cleanup. * Added many spaces * Shrink many brackets * 'and' for boolean => '&&' * 'or'  for boolean => '||'

--- lib/auth.php
+++ lib/auth.php
@@ -2,118 +2,108 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: auth.php,v 1.1 2004/08/01 01:54:35 henoheno Exp $
+// $Id: auth.php,v 1.2 2004/10/07 13:02:48 henoheno Exp $
 //
 
 // 編集不可能なページを編集しようとしたとき
-function check_editable($page,$auth_flag=TRUE,$exit_flag=TRUE)
+function check_editable($page, $auth_flag = TRUE, $exit_flag = TRUE)
 {
-	global $script,$_title_cannotedit,$_msg_unfreeze;
+	global $script, $_title_cannotedit, $_msg_unfreeze;
 
-	if (edit_auth($page,$auth_flag,$exit_flag) and is_editable($page))
-	{
+	if (edit_auth($page, $auth_flag, $exit_flag) && is_editable($page))
 		return TRUE;
-	}
-	if (!$exit_flag)
-	{
-		return FALSE;
-	}
-	$body = $title = str_replace('$1',htmlspecialchars(strip_bracket($page)),$_title_cannotedit);
-	if (is_freeze($page))
-	{
+
+	if (! $exit_flag) return FALSE;
+
+	$body = $title = str_replace('$1', htmlspecialchars(strip_bracket($page)), $_title_cannotedit);
+
+	if (is_freeze($page)) {
 		$body .= "(<a href=\"$script?cmd=unfreeze&amp;page=".
 			rawurlencode($page)."\">$_msg_unfreeze</a>)";
 	}
 
-	$page = str_replace('$1',make_search($page),$_title_cannotedit);
+	$page = str_replace('$1', make_search($page), $_title_cannotedit);
 
-	catbody($title,$page,$body);
+	catbody($title, $page, $body);
 	exit;
 }
 
 // 閲覧不可能なページを閲覧しようとしたとき (？)
-function check_readable($page,$auth_flag=TRUE,$exit_flag=TRUE)
+function check_readable($page, $auth_flag = TRUE, $exit_flag = TRUE)
 {
-	return read_auth($page,$auth_flag,$exit_flag);
+	return read_auth($page, $auth_flag, $exit_flag);
 }
 
 // 編集認証
-function edit_auth($page,$auth_flag=TRUE,$exit_flag=TRUE)
+function edit_auth($page, $auth_flag = TRUE, $exit_flag = TRUE)
 {
-	global $edit_auth,$edit_auth_pages,$_title_cannotedit;
+	global $edit_auth, $edit_auth_pages, $_title_cannotedit;
 
 	// 編集認証フラグをチェック
 	return $edit_auth ?
-		basic_auth($page,$auth_flag,$exit_flag,$edit_auth_pages,$_title_cannotedit) : TRUE;
+		basic_auth($page, $auth_flag, $exit_flag, $edit_auth_pages, $_title_cannotedit) : TRUE;
 }
 
 // 閲覧認証
-function read_auth($page,$auth_flag=TRUE,$exit_flag=TRUE)
+function read_auth($page, $auth_flag = TRUE, $exit_flag = TRUE)
 {
-	global $read_auth,$read_auth_pages,$_title_cannotread;
+	global $read_auth, $read_auth_pages, $_title_cannotread;
 
 	// 閲覧認証フラグをチェック
 	return $read_auth ?
-		basic_auth($page,$auth_flag,$exit_flag,$read_auth_pages,$_title_cannotread) : TRUE;
+		basic_auth($page, $auth_flag, $exit_flag, $read_auth_pages, $_title_cannotread) : TRUE;
 }
 
 // Basic認証
-function basic_auth($page,$auth_flag,$exit_flag,$auth_pages,$title_cannot)
+function basic_auth($page, $auth_flag, $exit_flag, $auth_pages, $title_cannot)
 {
-	global $auth_users,$auth_method_type;
+	global $auth_users, $auth_method_type;
 	global $_msg_auth;
 
-	// 認証要否判断対象文字列を取得する
+	// 認証要否判断対象
 	$target_str = '';
-	// ページ名でチェックする場合
-	if ($auth_method_type == 'pagename')
-	{
+	if ($auth_method_type == 'pagename') {
+		// ページ名でチェックする
 		$target_str = $page;
+	} else if ($auth_method_type == 'contents') {
+		// ページ内の文字列でチェックする
+		$target_str = join('', get_source($page));
 	}
-	// ページ内の文字列でチェックする場合
-	else if ($auth_method_type == 'contents')
-	{
-		$target_str = join('',get_source($page));
-	}
+
 	// 合致したパターンで定義されたユーザのリスト
 	$user_list = array();
-	foreach($auth_pages as $key=>$val)
-	{
-		if (preg_match($key,$target_str))
-		{
-			$user_list = array_merge($user_list,explode(',',$val));
-		}
+	foreach($auth_pages as $key=>$val) {
+		if (preg_match($key, $target_str))
+			$user_list = array_merge($user_list, explode(',', $val));
 	}
-	if (count($user_list) == 0)
-	{
-		// 制限なし
-		return TRUE;
-	}
+
+	if (count($user_list) == 0) return TRUE; // 制限なし
+
 	// PHP_AUTH* 変数が未定義の場合
+	$matches = array();
 	if (!isset($_SERVER['PHP_AUTH_USER'])
-		and !isset($_SERVER ['PHP_AUTH_PW'])
-		and isset($_SERVER['HTTP_AUTHORIZATION'])
-		and preg_match('/^Basic (.*)$/', $_SERVER['HTTP_AUTHORIZATION'],$matches))
+		&& ! isset($_SERVER ['PHP_AUTH_PW'])
+		&& isset($_SERVER['HTTP_AUTHORIZATION'])
+		&& preg_match('/^Basic (.*)$/', $_SERVER['HTTP_AUTHORIZATION'], $matches))
 	{
 		// HTTP_AUTHORIZATION 変数を使用した Basic 認証
 		list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode($matches[1]));
 	}
+
 	// ユーザリストに含まれるいずれかのユーザと認証されればOK
-	if (!isset($_SERVER['PHP_AUTH_USER'])
-		or !in_array($_SERVER['PHP_AUTH_USER'],$user_list)
-		or !array_key_exists($_SERVER['PHP_AUTH_USER'],$auth_users)
-		or $auth_users[$_SERVER['PHP_AUTH_USER']] != $_SERVER['PHP_AUTH_PW'])
+	if (! isset($_SERVER['PHP_AUTH_USER'])
+		|| ! in_array($_SERVER['PHP_AUTH_USER'], $user_list)
+		|| ! array_key_exists($_SERVER['PHP_AUTH_USER'], $auth_users)
+		|| $auth_users[$_SERVER['PHP_AUTH_USER']] != $_SERVER['PHP_AUTH_PW'])
 	{
-		if ($auth_flag)
-		{
+		if ($auth_flag) {
 			header('WWW-Authenticate: Basic realm="'.$_msg_auth.'"');
 			header('HTTP/1.0 401 Unauthorized');
 		}
-		if ($exit_flag)
-		{
-			$body = $title = str_replace('$1',htmlspecialchars(strip_bracket($page)),$title_cannot);
-			$page = str_replace('$1',make_search($page),$title_cannot);
-			catbody($title,$page,$body);
+		if ($exit_flag) {
+			$body = $title = str_replace('$1', htmlspecialchars(strip_bracket($page)), $title_cannot);
+			$page = str_replace('$1', make_search($page), $title_cannot);
+			catbody($title, $page, $body);
 			exit;
 		}
 		return FALSE;
