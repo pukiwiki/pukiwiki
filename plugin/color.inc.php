2d698476dee3db385b6417f1adbf0f410e55869c
henoheno <henoheno>
2004-11-21 18:55:02 +0900

    Overhaul. 1.4.x user must replace this

--- plugin/color.inc.php
+++ plugin/color.inc.php
@@ -2,47 +2,45 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: color.inc.php,v 1.9 2004/07/31 03:09:20 henoheno Exp $
+// $Id: color.inc.php,v 1.10 2004/11/21 09:55:02 henoheno Exp $
 //
+// Text color plugin
 
+define('PLUGIN_COLOR_ALLOW_CSS', FALSE); // TRUE, FALSE
+
+// ----
+define('PLUGIN_COLOR_REGEX', '/^(#[0-9a-f]{6}|[a-z-]+)$/i');
 function plugin_color_inline()
 {
-	if (func_num_args() == 3)
-	{
-		list($color,$bgcolor,$body) = func_get_args();
-		if ($body == '')
-		{
-			$body = $bgcolor;
-			$bgcolor = '';
-		}
-	}
-	else if (func_num_args() == 2)
-	{
+	static $usage = '&color(foreground[,background]){text};';
+	global $html_transitional;
+
+	$args = func_get_args();
+	$text = array_pop($args); // htmlspecialchars(text)
+
+	list($color, $bgcolor) = array_pad($args, 2, '');
+	if ($text == '' && $bgcolor != '') {
+		// Maybe the old style: '&color(foreground,text);'
+		$text    = htmlspecialchars($bgcolor);
 		$bgcolor = '';
-		list($color,$body) = func_get_args();
 	}
-	else
-	{
-		return FALSE;
+	if ($color == '' || $text == '' || func_num_args() > 3) return $usage;
+
+	// Invalid color
+	foreach(array($color, $bgcolor) as $col){
+		if ($col != '' && ! preg_match(PLUGIN_COLOR_REGEX, $col))
+			return '&color():Invalid color: ' . htmlspecialchars($col) . ';';
 	}
 
-	if ($color == '' or $body == '')
-	{
-		return FALSE;
+	if ($html_transitional === TRUE && PLUGIN_COLOR_ALLOW_CSS === TRUE) {
+		if ($bgcolor != '') $bgcolor = ';background-color:' . $bgcolor;
+		return '<span style="color:' . $color . $bgcolor . '">' . $text . '</span>';
+	} else {
+		// Using <font> tag with:
+		//   NG: XHTML 1.1
+		//   OK: XHTML 1.0 Transitional
+		if ($bgcolor != '') return '&color(): bgcolor (with CSS) not allowd;';
+		return '<font color="' . $color . '">' . $text . '</font>';
 	}
-	if (!plugin_color_is_valid($color) or !plugin_color_is_valid($bgcolor))
-	{
-		return $body;
-	}
-
-	if ($bgcolor != '')
-	{
-		$color .= ';background-color:'.$bgcolor;
-	}
-	return "<span style=\"color:$color\">$body</span>";
-}
-function plugin_color_is_valid($color)
-{
-	return ($color == '') or preg_match('/^(#[0-9a-f]+|[\w-]+)$/i',$color);
 }
 ?>
