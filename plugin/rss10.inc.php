5abc85da0dc5fb813fb80c6ab4903f003beec22d
henoheno <henoheno>
2004-11-28 13:52:43 +0900

    Status: 301 Moved Permanently

--- plugin/rss10.inc.php
+++ plugin/rss10.inc.php
@@ -1,19 +1,10 @@
 <?php
-/////////////////////////////////////////////////
-// PukiWiki - Yet another WikiWikiWeb clone.
-//
-// $Id: rss10.inc.php,v 1.13 2004/11/08 12:01:37 henoheno Exp $
-//
-// This plugin had been merged into rss plugin.
-// Please use it instead of.
-
-// Compat
+// RSS 1.0 plugin - had been merged into rss plugin
+// $Id: rss10.inc.php,v 1.14 2004/11/28 04:52:43 henoheno Exp $
 function plugin_rss10_action()
 {
-	global $vars;
-	exist_plugin('rss') or die('rss plugin not found');
-
-	$vars['ver'] = '1.0';
-	plugin_rss_action();
+	header('Status: 301 Moved Permanently');
+	header('Location: ' . $script . '?cmd=rss&ver=1.0'); // HTTP
+	exit;
 }
 ?>
