97dde208c4c135e63aa053b957ee96dae98a8ea0
henoheno <henoheno>
2004-11-28 21:50:13 +0900

    Add a blank line only

--- plugin/rss10.inc.php
+++ plugin/rss10.inc.php
@@ -1,6 +1,7 @@
 <?php
 // RSS 1.0 plugin - had been merged into rss plugin
-// $Id: rss10.inc.php,v 1.14 2004/11/28 04:52:43 henoheno Exp $
+// $Id: rss10.inc.php,v 1.15 2004/11/28 12:50:13 henoheno Exp $
+
 function plugin_rss10_action()
 {
 	header('Status: 301 Moved Permanently');
