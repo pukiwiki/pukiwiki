e2481f47f13bd5bbe0030dd51c7ce8831f272141
henoheno <henoheno>
2004-11-28 15:31:04 +0900

    Overhaul. Don't show MD5 hash and its key at the same time. Don't use GET method sending key

--- plugin/md5.inc.php
+++ plugin/md5.inc.php
@@ -1,21 +1,32 @@
 <?php
-/////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
-//
-// $Id: md5.inc.php,v 1.4 2004/07/31 03:09:20 henoheno Exp $
-//
-//  MD5パスワードへの変換
+// $Id: md5.inc.php,v 1.5 2004/11/28 06:31:04 henoheno Exp $
+//  MD5 plugin
+
 function plugin_md5_action()
 {
-	global $vars;
+	global $script, $get, $post;
 
-	if (!array_key_exists('md5',$vars))
-	{
-		return FALSE;
+	// Wait POST
+	$key = isset($post['key']) ? $post['key'] : '';
+	if ($key != '') {
+		// Compute (Don't show its $key at the same time)
+		return array('msg'=>'MD5', 'body'=>'Hash: ' . md5($key));
+	} else {
+		// If cmd=md5&md5=password, set only (Don't compute)
+		$value = isset($get['md5']) ? $get['md5'] : '';
+		if ($value != '') $value  = 'value="' . htmlspecialchars($value) . '" ';
+		$form = <<<EOD
+<form action="$script" method="post">
+ <div>
+  <input type="hidden" name="plugin" value="md5" />
+  <input type="text"   name="key" size="30" $value/>
+  <input type="submit" value="Compute" />
+ </div>
+</form>
+<br/>
+EOD;
+		return array('msg'=>'MD5', 'body'=>$form);
 	}
-	return array(
-		'msg'=>'Make password of MD5',
-		'body'=> htmlspecialchars($vars['md5']).' : '.md5($vars['md5'])
-	);
 }
 ?>
