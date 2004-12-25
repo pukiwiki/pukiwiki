4e729ac0f8b715bfb958b402ebb09f0ce1a61cd1
henoheno <henoheno>
2004-12-25 23:31:30 +0900

    Cleanup. Shrink. Rename defines. Added spaces. Show usage when '#navi' with no child page. Japanese => English.

--- plugin/navi.inc.php
+++ plugin/navi.inc.php
@@ -1,155 +1,145 @@
 <?php
-/////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: navi.inc.php,v 1.17 2004/12/25 14:31:30 henoheno Exp $
 //
-// $Id: navi.inc.php,v 1.16 2004/07/31 03:09:20 henoheno Exp $
-//
+// Navi plugin: Show DocBook-like navigation bar and contents
 
 /*
+ * Usage:
+ *   #navi(contents-page-name)   <for ALL child pages>
+ *   #navi([contents-page-name]) <for contents page>
+ *
+ * Parameter:
+ *   contents-page-name - Page name of home of the navigation (default:itself)
+ *
+ * Behaviour at contents page:
+ *   Always show child-page list like 'ls' plugin
+ *
+ * Behaviour at child pages:
+ *
+ *   The first plugin call - Show a navigation bar like a DocBook header
+ *
+ *     Prev  <contents-page-name>  Next
+ *     --------------------------------
+ *
+ *   The second call - Show a navigation bar like a DocBook footer
+ *
+ *     --------------------------------
+ *     Prev          Home          Next
+ *     <pagename>     Up     <pagename>
+ *
+ * Page-construction example:
+ *   foobar    - Contents page, includes '#navi' or '#navi(foobar)'
+ *   foobar/1  - One of child pages, includes one or two '#navi(foobar)'
+ *   foobar/2  - One of child pages, includes one or two '#navi(foobar)'
+ */
 
-*プラグイン navi
-DobBook風のナビゲーションバーを表示する
+// Exclusive regex pattern of child pages
+define('PLUGIN_NAVI_EXCLUSIVE_REGEX', '');
+//define('PLUGIN_NAVI_EXCLUSIVE_REGEX', '#/_#'); // Ignore 'foobar/_memo' etc.
 
-*Usage
- #navi(page)
+// Insert <link rel=... /> tags into XHTML <head></head>
+define('PLUGIN_NAVI_LINK_TAGS', FALSE);	// FALSE, TRUE
 
-*パラメータ
--page~
- HOMEとなるページ。省略すると自ページをHOMEとする。
-
-*動作
-
--1回目の参照(HOME)~
- ページ一覧をls.inc.php風に表示する
--1回目の参照(HOME以外)
- header : ヘッダ風
-  prev       next
- -----------------
--2回目の参照
- footer : フッタ風
- ------------------
-  prev  home  next
-  title  up  title
-
-*/
-
-// 除外するページ (正規表現で)
-define('NAVI_EXCLUDE_PATTERN','');
-#define('NAVI_EXCLUDE_PATTERN','/\/_/');
-
-// <link>タグを出力する (TRUE|FALSE)
-define('NAVI_LINK_TAGS',FALSE);
+// ----
 
 function plugin_navi_convert()
 {
 	global $vars, $script, $head_tags;
-	global $_navi_prev,$_navi_next,$_navi_up,$_navi_home;
+	global $_navi_prev, $_navi_next, $_navi_up, $_navi_home;
 	static $navi = array();
 
-	$home = $current = $vars['page'];
-	if (func_num_args())
-	{
+	if (func_num_args()) {
 		list($home) = func_get_args();
 		$home = strip_bracket($home);
+	} else {
+		$home = $vars['page'];
 	}
+	$current = $vars['page'];
 	$is_home = ($home == $current);
 
-	// 初回FALSE,2回目以降TRUE
-	$footer = array_key_exists($home,$navi);
-	if (!$footer)
-	{
+	$footer = isset($navi[$home]); // The first time: FALSE, the second: TRUE
+	if (! $footer) {
 		$navi[$home] = array(
-			'up'=>'',
-			'prev'=>'',
+			'up'   =>'',
+			'prev' =>'',
 			'prev1'=>'',
-			'next'=>'',
+			'next' =>'',
 			'next1'=>'',
-			'home'=>'',
+			'home' =>'',
 			'home1'=>'',
 		);
 
-		$pages = preg_grep('/^'.preg_quote($home,'/').'($|\/)/',get_existpages());
-		// preg_grep(,,PREG_GREP_INVERT)が使えれば…
-		if (NAVI_EXCLUDE_PATTERN != '')
-		{
-			$pages = array_diff($pages,preg_grep(NAVI_EXCLUDE_PATTERN,$pages));
+		$pages = preg_grep('/^' . preg_quote($home, '/') .
+			'($|\/)/', get_existpages());
+		if (PLUGIN_NAVI_EXCLUSIVE_REGEX != '') {
+			// If old PHP could use preg_grep(,,PREG_GREP_INVERT)...
+			$pages = array_diff($pages,
+				preg_grep(PLUGIN_NAVI_EXCLUSIVE_REGEX, $pages));
 		}
-		$pages[] = $current; // 番兵 :)
-		$pages = array_unique($pages);
+		$pages[] = $current; // Sentinel :)
+		$pages   = array_unique($pages);
 		natcasesort($pages);
 		$prev = $home;
-		foreach ($pages as $page)
-		{
-			if ($page == $current)
-			{
-				break;
-			}
+		foreach ($pages as $page) {
+			if ($page == $current) break;
 			$prev = $page;
 		}
 		$next = current($pages);
 
 		$pos = strrpos($current, '/');
 		$up = '';
-		if ($pos > 0)
-		{
+		if ($pos > 0) {
 			$up = substr($current, 0, $pos);
-			$navi[$home]['up'] = make_pagelink($up,$_navi_up);
+			$navi[$home]['up']    = make_pagelink($up, $_navi_up);
 		}
-		if (!$is_home)
-		{
-			$navi[$home]['prev'] = make_pagelink($prev);
-			$navi[$home]['prev1'] = make_pagelink($prev,$_navi_prev);
+		if (! $is_home) {
+			$navi[$home]['prev']  = make_pagelink($prev);
+			$navi[$home]['prev1'] = make_pagelink($prev, $_navi_prev);
 		}
-		if ($next != '')
-		{
-			$navi[$home]['next'] = make_pagelink($next);
-			$navi[$home]['next1'] = make_pagelink($next,$_navi_next);
+		if ($next != '') {
+			$navi[$home]['next']  = make_pagelink($next);
+			$navi[$home]['next1'] = make_pagelink($next, $_navi_next);
 		}
-		$navi[$home]['home'] = make_pagelink($home);
-		$navi[$home]['home1'] = make_pagelink($home,$_navi_home);
+		$navi[$home]['home']  = make_pagelink($home);
+		$navi[$home]['home1'] = make_pagelink($home, $_navi_home);
 
-		// <link>タグを生成する : start next prev(previous) parent(up)
-		// 未対応 : contents(toc) search first(begin) last(end)
-		if (NAVI_LINK_TAGS)
-		{
-			foreach (array('start'=>$home,'next'=>$next,'prev'=>$prev,'up'=>$up) as $rel=>$_page)
-			{
-				if ($_page != '')
-				{
+		// Generate <link> tag: start next prev(previous) parent(up)
+		// Not implemented: contents(toc) search first(begin) last(end)
+		if (PLUGIN_NAVI_LINK_TAGS) {
+			foreach (array('start'=>$home, 'next'=>$next,
+			    'prev'=>$prev, 'up'=>$up) as $rel=>$_page) {
+				if ($_page != '') {
 					$s_page = htmlspecialchars($_page);
 					$r_page = rawurlencode($_page);
-					$head_tags[] = " <link rel=\"$rel\" href=\"$script?$r_page\" title=\"$s_page\" />";
+					$head_tags[] = ' <link rel="' .
+						$rel . '" href="' . $script .
+						'?' . $r_page . '" title="' .
+						$s_page . '" />';
 				}
 			}
 		}
 	}
 
 	$ret = '';
-	if ($footer) // フッタ
-	{
-		$ret = <<<EOD
-<hr class="full_hr" />
-<ul class="navi">
- <li class="navi_left">{$navi[$home]['prev1']}<br />{$navi[$home]['prev']}</li>
- <li class="navi_right">{$navi[$home]['next1']}<br />{$navi[$home]['next']}</li>
- <li class="navi_none">{$navi[$home]['home1']}<br />{$navi[$home]['up']}</li>
-</ul>
-EOD;
-	}
-	else if ($is_home) // 目次
-	{
-		$ret .= '<ul>';
-		foreach ($pages as $page)
-		{
-			if ($page != $home)
-			{
-				$ret .= ' <li>'.make_pagelink($page).'</li>';
-			}
+
+	if ($is_home) {
+		// Contents
+		if (count($pages) == 1) {
+			// Sentinel only: Show usage and warning
+			$home = htmlspecialchars($home);
+			$ret .= '#navi(' . $home . '): No child page like: ' .
+				$home . '/Foo';
+		} else {
+			$ret .= '<ul>';
+			foreach ($pages as $page)
+				if ($page != $home)
+					$ret .= ' <li>' . make_pagelink($page) . '</li>';
+			$ret .= '</ul>';
 		}
-		$ret .= '</ul>';
-	}
-	else // ヘッダ
-	{
+
+	} else if (! $footer) {
+		// Header
 		$ret = <<<EOD
 <ul class="navi">
  <li class="navi_left">{$navi[$home]['prev1']}</li>
@@ -157,6 +147,17 @@ EOD;
  <li class="navi_none">{$navi[$home]['home']}</li>
 </ul>
 <hr class="full_hr" />
+EOD;
+
+	} else {
+		// Footer
+		$ret = <<<EOD
+<hr class="full_hr" />
+<ul class="navi">
+ <li class="navi_left">{$navi[$home]['prev1']}<br />{$navi[$home]['prev']}</li>
+ <li class="navi_right">{$navi[$home]['next1']}<br />{$navi[$home]['next']}</li>
+ <li class="navi_none">{$navi[$home]['home1']}<br />{$navi[$home]['up']}</li>
+</ul>
 EOD;
 	}
 	return $ret;
