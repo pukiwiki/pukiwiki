9a457e2b98ba58ccfb505a4ce04c8b85eda464f2
henoheno <henoheno>
2004-12-30 18:21:05 +0900

    A little cleanup. Added \n into usages. Some English => Japanese. Removed debugging echo()es.

--- plugin/calendar_viewer.inc.php
+++ plugin/calendar_viewer.inc.php
@@ -1,46 +1,37 @@
 <?php
 /*
- * PukiWiki calendar_viewerプラグイン
- * $Id: calendar_viewer.inc.php,v 1.25 2004/08/11 14:19:48 henoheno Exp $
- * calendarrecentプラグインを元に作成
+ * PukiWiki calendar_viewer plubin
+ * $Id: calendar_viewer.inc.php,v 1.26 2004/12/30 09:21:05 henoheno Exp $
+ * Based on calendarrecent plugin
  */
 
-/**
+/*
  * 概要
-  calendarプラグインやcalendar2プラグインで作成したページを一覧表示するためのプラグインです。
-
+ * calendarプラグインやcalendar2プラグインで作成したページを一覧表示するためのプラグインです。
  * 使い方
-    #calendar_viewer(pagename,(this|yyyy-mm|n|x*y),[mode],[separater])
-
+ *  #calendar_viewer(pagename,(this|yyyy-mm|n|x*y),[mode],[separater])
+ *
  ** pagename
-  calendar or calendar2プラグインを記述してるページ名
-
+ * - calendar or calendar2プラグインを記述してるページ名
+ *
  ** (yyyy-mm|n|this)
-  - this
-  -- 今月のページを一覧表示
-  - yyyy-mm
-  -- yyyy-mmで指定した年月のページを一覧表示
-  - n
-  -- 先頭からn件を一覧表示
-  - x*n
-  -- 先頭より数えて x ページ目(先頭は0)から、y件づつ一覧表示
-
+ * this    - 今月のページを一覧表示
+ * yyyy-mm - yyyy-mmで指定した年月のページを一覧表示
+ * n       - 先頭からn件を一覧表示
+ * x*n     - 先頭より数えて x ページ目(先頭は0)から、y件づつ一覧表示
+ *
  ** [mode]
-  省略可能です。省略時のデフォルトはpast
-  - past
-  -- 今日以前のページの一覧表示モード。更新履歴や日記向き
-  - future
-  -- 今日以降のページの一覧表示モード。イベント予定やスケジュール向き
-  - view
-  -- 過去から未来への一覧表示モード。表示抑止するページはありません
-
+ * Default:past
+ * past   - 今日以前のページの一覧表示モード。更新履歴や日記向き
+ * future - 今日以降のページの一覧表示モード。イベント予定やスケジュール向き
+ * view   - 過去から未来への一覧表示モード。表示抑止するページはありません
+ *
  ** [separater]
-  - 年月日を区切るセパレータを指定。
-  - 省略可能。デフォルトは-。（calendar2なら省略でOK）
-
+ * - 年月日を区切るセパレータを指定。
+ * - 省略可能。デフォルトは-。（calendar2なら省略でOK）
+ *
  * TODO
-  past or future で月単位表示するときに、それぞれ来月、先月の一覧へのリンクを表示しないようにする
-
+ *  past or future で月単位表示するときに、それぞれ来月、先月の一覧へのリンクを表示しないようにする
  */
 
 function plugin_calendar_viewer_convert()
@@ -52,13 +43,12 @@ function plugin_calendar_viewer_convert()
 
 	static $viewed = array();
 
-	// 引数の確認
 	if (func_num_args() < 2)
-		return '#calendar_viewer(): ' . $_err_calendar_viewer_param . '<br />';
+		return '#calendar_viewer(): ' . $_err_calendar_viewer_param . '<br />' . "\n";
 
 	$func_args = func_get_args();
 
-	// デフォルト値
+	// Default values
 	$pagename    = $func_args[0];	// 基準となるページ名
 	$page_YM     = '';	// 一覧表示する年月
 	$limit_base  = 0;	// 先頭から数えて何ページ目から表示するか (先頭)
@@ -67,7 +57,7 @@ function plugin_calendar_viewer_convert()
 	$mode        = 'past';	// 動作モード
 	$date_sep    = '-';	// 日付のセパレータ calendar2なら '-', calendarなら ''
 
-	// $func_args[1] のチェック
+	// Check $func_args[1]
 	if (preg_match('/[0-9]{4}' . $date_sep . '[0-9]{2}/', $func_args[1])) {
 		// 指定年月の一覧表示
 		$page_YM     = $func_args[1];
@@ -86,18 +76,15 @@ function plugin_calendar_viewer_convert()
 		$limit_pitch = $matches[2];
 		$limit_page  = $matches[1] + $matches[2]; // 読み飛ばす + 表示する
 	} else {
-		return '#calendar_viewer(): ' . $_err_calendar_viewer_param2 . '<br />';
+		return '#calendar_viewer(): ' . $_err_calendar_viewer_param2 . '<br />' . "\n";
 	}
 
-	if (isset($func_args[2]) &&
-	    preg_match('/^(past|view|future)$/si', $func_args[2])) {
-		// モード指定
+	// $func_args[2]: Mode setting
+	if (isset($func_args[2]) && preg_match('/^(past|view|future)$/si', $func_args[2]))
 		$mode = $func_args[2];
-	}
 
-	if (isset($func_args[3])) {
-		$date_sep = $func_args[3];
-	}
+	// $func_args[3]: Change default delimiter
+	if (isset($func_args[3])) $date_sep = $func_args[3];
 
 	// Avoid Loop etc.
 	if (isset($viewed[$pagename])) {
@@ -121,53 +108,42 @@ function plugin_calendar_viewer_convert()
 		$filepattern_len = strlen($filepattern);
 	}
 
-	//echo "$pagename:$page_YM:$mode:$date_sep:$limit_base:$limit_page";
 	// ページリストの取得
-	//echo $pagepattern;
-	//echo $filepattern;
 	$pagelist = array();
 	if ($dir = @opendir(DATA_DIR)) {
 		$_date = get_date('Y' . $date_sep . 'm' . $date_sep . 'd');
+		$page_date  = '';
 		while($file = readdir($dir)) {
 			if ($file == '..' || $file == '.') continue;
 			if (substr($file, 0, $filepattern_len) != $filepattern) continue;
 
-			//echo "OK";
-			$page = decode(trim(preg_replace('/\.txt$/', ' ', $file)));
+			$page      = decode(trim(preg_replace('/\.txt$/', ' ', $file)));
+			$page_date = substr($page, $pagepattern_len);
 
-			// $pageがカレンダー形式なのかチェック デフォルトでは、 yyyy-mm-dd
-			$page = strip_bracket($page);
-			if (plugin_calendar_viewer_isValidDate(substr($page, $pagepattern_len), $date_sep) == false) continue;
+			// Verify the $page_date pattern (Default: yyyy-mm-dd).
+			// Past-mode hates the future, and
+			// Future-mode hates the past.
+			if ((plugin_calendar_viewer_isValidDate($page_date, $date_sep) == FALSE) || 
+				($page_date > $_date && ($mode == 'past')) ||
+				($page_date < $_date && ($mode == 'future')))
+					continue;
 
-			// mode毎に別条件ではじく
-
-			// past modeでは未来のページはNG
-			if (((substr($page, $pagepattern_len)) > $_date) &&
-			    ($mode == 'past')) continue;
-
-			// future modeでは過去のページはNG
-			if (((substr($page, $pagepattern_len)) < $_date) &&
-				($mode == 'future')) continue;
-
-			// view modeならall OK
 			$pagelist[] = $page;
 		}
 	}
 	closedir($dir);
-	//echo count($pagelist);
 
-	// ソート
 	if ($mode == 'past') {
-		rsort($pagelist);	// past modeでは新→旧
+		rsort($pagelist);	// New => Old
 	} else {
-		sort($pagelist);	// view mode と future mode では、旧→新
+		sort($pagelist);	// Old => New
 	}
 
-	// ここからインクルード開始
-	$tmppage = $vars['page'];
+	// Include start
+	$tmppage     = $vars['page'];
 	$return_body = '';
 
-	// $limit_pageの件数までインクルード
+	// $limit_page の件数までインクルード
 	$tmp = max($limit_base, 0); // Skip minus
 	while ($tmp < $limit_page) {
 		if (! isset($pagelist[$tmp])) break;
@@ -176,7 +152,7 @@ function plugin_calendar_viewer_convert()
 		$get['page'] = $post['page'] = $vars['page'] = $page;
 
 		// 現状で閲覧許可がある場合だけ表示する
-		if (check_readable($page, false, false)) {
+		if (check_readable($page, FALSE, FALSE)) {
 			$body = convert_html(get_source($page));
 		} else {
 			$body = str_replace('$1', $page, $_msg_calendar_viewer_restrict);
@@ -184,8 +160,8 @@ function plugin_calendar_viewer_convert()
 
 		$r_page = rawurlencode($page);
 		$s_page = htmlspecialchars($page);
-		$link   = "<a href=\"$script?cmd=edit&amp;page=$r_page\">$s_page</a>";
-		$head   = "<h1>$link</h1>\n";
+		$link   = '<a href="' . $script . '?cmd=edit&amp;page=' . $r_page . '">' . $s_page . '</a>';
+		$head   = '<h1>' . $link . '</h1>' . "\n";
 		$return_body .= $head . $body;
 
 		++$tmp;
@@ -250,16 +226,14 @@ function plugin_calendar_viewer_convert()
 	if ($left_YM != '' || $right_YM != '') {
 		$s_date_sep = htmlspecialchars($date_sep);
 		$left_link = $right_link = '';
-		$link = "$script?plugin=calendar_viewer&amp;mode=$mode&amp;" .
-			"file=$enc_pagename&amp;date_sep=$s_date_sep&amp;";
-		if ($left_YM != '') {
+		$link = $script . '?plugin=calendar_viewer&amp;mode=' . $mode .
+			'&amp;file=' . $enc_pagename . '&amp;date_sep=' . $s_date_sep . '&amp;';
+		if ($left_YM != '')
 			$left_link = '<a href="' . $link .
-				"date=$left_YM\">$left_text</a>";
-		}
-		if ($right_YM != '') {
+				'date=' . $left_YM . '">' . $left_text . '</a>';
+		if ($right_YM != '')
 			$right_link = '<a href="' . $link .
-				"date=$right_YM\">$right_text</a>";
-		}
+				'date=' . $right_YM . '">' . $right_text . '</a>';
 		// past modeは<<新 旧>> 他は<<旧 新>>
 		$return_body .=
 			'<table width ="100%"><tr>' .
@@ -288,9 +262,7 @@ function plugin_calendar_viewer_action()
 	$date_sep = $vars['date_sep'];
 
 	$page_YM = $vars['date'];
-	if ($page_YM == '') {
-		$page_YM = get_date('Y' . $date_sep . 'm');
-	}
+	if ($page_YM == '') $page_YM = get_date('Y' . $date_sep . 'm');
 	$mode = $vars['mode'];
 
 	$args_array = array($vars['page'], $page_YM, $mode, $date_sep);
@@ -298,9 +270,7 @@ function plugin_calendar_viewer_action()
 
 	//$return_vars_array['msg'] = 'calendar_viewer ' . $vars['page'] . '/' . $page_YM;
 	$return_vars_array['msg'] = 'calendar_viewer ' . htmlspecialchars($vars['page']);
-	if ($vars['page'] != '') {
-		$return_vars_array['msg'] .= '/';
-	}
+	if ($vars['page'] != '') $return_vars_array['msg'] .= '/';
 	if (preg_match('/\*/', $page_YM)) {
 		// うーん、n件表示の時はなんてページ名にしたらいい？
 	} else {
@@ -314,13 +284,13 @@ function plugin_calendar_viewer_action()
 function plugin_calendar_viewer_isValidDate($aStr, $aSepList = '-/ .')
 {
 	if ($aSepList == '') {
-		// $aSepList = ''なら、yyyymmddとしてチェック（手抜き(^^;）
+		// yyymmddとしてチェック（手抜き(^^;）
 		return checkdate(substr($aStr, 4, 2), substr($aStr, 6, 2), substr($aStr, 0, 4));
-	}
-	if ( ereg("^([0-9]{2,4})[$aSepList]([0-9]{1,2})[$aSepList]([0-9]{1,2})$", $aStr, $m) ) {
+	} else if (ereg("^([0-9]{2,4})[$aSepList]([0-9]{1,2})[$aSepList]([0-9]{1,2})$", $aStr, $m) ) {
 		return checkdate($m[2], $m[3], $m[1]);
+	} else {
+		return FALSE;
 	}
-	return false;
 }
 
 ?>
