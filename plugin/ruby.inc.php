b8d6a290e931abf34fbdc62e82e58a4608f727db
henoheno <henoheno>
2005-01-04 23:25:24 +0900

    Cleanup. Added comments. Shrink. Show usage

--- plugin/ruby.inc.php
+++ plugin/ruby.inc.php
@@ -1,25 +1,25 @@
 <?php
-/////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: ruby.inc.php,v 1.5 2005/01/04 14:25:24 henoheno Exp $
 //
-// $Id: ruby.inc.php,v 1.4 2004/07/31 03:09:20 henoheno Exp $
+// Ruby annotation plugin: Add a pronounciation into kanji-word or acronym(s)
+// See also about ruby: http://www.w3.org/TR/ruby/
 //
+// NOTE:
+//  Ruby tag works with MSIE only now,
+//  but readable for other browsers like: 'words(pronunciation)'
+
+define('PLUGIN_RUBY_USAGE', '&ruby(pronunciation){words};');
 
 function plugin_ruby_inline()
 {
-	if (func_num_args() != 2)
-	{
-		return FALSE;
-	}
+	if (func_num_args() != 2) return PLUGIN_RUBY_USAGE;
 
-	list($ruby,$body) = func_get_args();
+	list($ruby, $body) = func_get_args();
+	if ($ruby == '' || $body == '') return PLUGIN_RUBY_USAGE;
 
-	if ($ruby == '' or $body == '')
-	{
-		return FALSE;
-	}
-
-	$s_ruby = htmlspecialchars($ruby);
-	return "<ruby><rb>$body</rb><rp>(</rp><rt>$s_ruby</rt><rp>)</rp></ruby>";
+	return '<ruby><rb>' . $body . '</rb>' . '<rp>(</rp>' .
+		'<rt>' .  htmlspecialchars($ruby) . '</rt>' . '<rp>)</rp>' .
+		'</ruby>';
 }
 ?>
