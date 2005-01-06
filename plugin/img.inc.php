d64bf005f8f8fe8c643d158cc39906896c31e165
henoheno <henoheno>
2005-01-06 21:40:56 +0900

    Cleanup. A value => a define

--- plugin/img.inc.php
+++ plugin/img.inc.php
@@ -1,17 +1,15 @@
 <?php
-/////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: img.inc.php,v 1.13 2005/01/06 12:40:56 henoheno Exp $
 //
-// $Id: img.inc.php,v 1.12 2004/08/23 11:41:57 henoheno Exp $
-//
+// Inline-image plugin
 
-// Stop word-wrapping
-define('PLUGIN_IMG_CLEAR', "<div style=\"clear:both\"></div>\n");
+define('PLUGIN_IMG_USAGE', '#img(): Usage: (URI-to-image[,right[,clear]])<br />' . "\n");
+define('PLUGIN_IMG_CLEAR', '<div style="clear:both"></div>' . "\n"); // Stop word-wrapping
 
 // 画像をインライン表示
 function plugin_img_convert()
 {
-	$usage = "#img(): Usage: (URI-to-image[,right[,clear]])<br />\n";
 	$args = func_get_args();
 
 	// Check the 2nd argument first, for compatibility
@@ -28,7 +26,7 @@ function plugin_img_convert()
 
 	$url = isset($args[0]) ? $args[0] : '';
 	if (! is_url($url) || ! preg_match('/\.(jpe?g|gif|png)$/i', $url))
-		return $usage;
+		return PLUGIN_IMG_USAGE;
 
 	$arg = isset($args[2]) ? strtoupper($args[2]) : '';
 	$clear = ($arg == 'C' || $arg == 'CLEAR') ? PLUGIN_IMG_CLEAR : '';
