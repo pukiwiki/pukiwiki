fd36ef853e73bd920d197cb32b8d5ee5d08daf89
henoheno <henoheno>
2005-01-22 12:35:54 +0900

    Simplify

--- plugin/version.inc.php
+++ plugin/version.inc.php
@@ -1,10 +1,14 @@
 <?php
-// $Id: version.inc.php,v 1.6 2003/01/27 05:38:47 panda Exp $
+// PukiWiki - Yet another WikiWikiWeb clone
+// $Id: version.inc.php,v 1.7 2005/01/22 03:35:54 henoheno Exp $
+//
+// Show PukiWiki version
 
 function plugin_version_convert()
 {
-	return '<p>'.plugin_version_inline().'</p>';
+	return '<p>' . S_VERSION . '</p>';
 }
+
 function plugin_version_inline()
 {
 	return S_VERSION;
