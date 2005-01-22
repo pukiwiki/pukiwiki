1d3380f6e67cd1dc03d3ef4c703ad3d4bc4535b1
henoheno <henoheno>
2005-01-22 13:22:01 +0900

    Enable to list DIFF_DIR. Japanese => English. Add comments about CACHE_DIR. Simplify.

--- plugin/deleted.inc.php
+++ plugin/deleted.inc.php
@@ -1,27 +1,43 @@
 <?php
-/////////////////////////////////////////////////
-// PukiWiki - Yet another WikiWikiWeb clone.
+// PukiWiki - Yet another WikiWikiWeb clone
+// $Id: deleted.inc.php,v 1.6 2005/01/22 04:22:01 henoheno Exp $
 //
-// $Id: deleted.inc.php,v 1.5 2004/07/31 03:09:20 henoheno Exp $
+// Show deleted (= Exists in BACKUP_DIR or DIFF_DIR but not in DATA_DIR)
+// page list to clean them up
 //
-//削除されたページ(BACKUP_DIRにあって、DATA_DIRにないファイル)の一覧を表示する
+// Usage:
+//   index.php?plugin=deleted[&file=on]
+//   index.php?plugin=deleted&dir=diff[&file=on]
 
 function plugin_deleted_action()
 {
 	global $vars;
-	global $_deleted_plugin_title,$_deleted_plugin_title_withfilename;
+	global $_deleted_plugin_title, $_deleted_plugin_title_withfilename;
 
-	$retval = array();
+	$dir = isset($vars['dir']) ? $vars['dir'] : 'backup';
+	$withfilename  = isset($vars['file']);
 
-	$retval['msg'] = $_deleted_plugin_title;
-	if ($withfilename = array_key_exists('file',$vars))
-	{
+	$_DIR['diff'  ]['dir'] = DIFF_DIR;
+	$_DIR['diff'  ]['ext'] = '.txt';
+	$_DIR['backup']['dir'] = BACKUP_DIR;
+	$_DIR['backup']['ext'] = BACKUP_EXT; // .gz or .txt
+	//$_DIR['cache' ]['dir'] = CACHE_DIR; // No way to delete them via web browser now
+	//$_DIR['cache' ]['ext'] = '.ref';
+	//$_DIR['cache' ]['ext'] = '.rel';
+
+	if (! isset($_DIR[$dir]))
+		return array('msg'=>'Deleted plugin', 'body'=>'No such setting: Choose backup or diff');
+
+	$deleted_pages  = array_diff(
+		get_existpages($_DIR[$dir]['dir'], $_DIR[$dir]['ext']),
+		get_existpages());
+
+	if ($withfilename) {
 		$retval['msg'] = $_deleted_plugin_title_withfilename;
+	} else {
+		$retval['msg'] = $_deleted_plugin_title;
 	}
-	$backup_pages = get_existpages(BACKUP_DIR,BACKUP_EXT);
-	$exist_pages = get_existpages();
-	$deleted_pages = array_diff($backup_pages,$exist_pages);
-	$retval['body'] = page_list($deleted_pages,'backup',$withfilename);
+	$retval['body'] = page_list($deleted_pages, $dir, $withfilename);
 
 	return $retval;
 }
