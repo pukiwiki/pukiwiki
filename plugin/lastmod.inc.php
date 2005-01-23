03e3ee8fd7c481a8cd760f5a3f515f89bb00fa67
henoheno <henoheno>
2005-01-23 22:02:08 +0900

    Newly added lastmod plugin: Originally written by Reimy

---
+++ plugin/lastmod.inc.php
@@ -0,0 +1,28 @@
+<?php
+// PukiWiki - Yet another WikiWikiWeb clone
+// $Id: lastmod.inc.php,v 1.1 2005/01/23 13:02:08 henoheno Exp $
+//
+// Lastmod plugin - Show lastmodifled date of the page
+// Originally written by Reimy
+
+function plugin_lastmod_inline()
+{
+	global $vars, $WikiName, $BracketName;
+
+	$args = func_get_args();
+	$page = $args[0];
+
+	if ($page == ''){
+		$page = $vars['page']; // Default: page itself
+	} else {
+		if (preg_match("/^($WikiName|\[\[$BracketName\]\])$/", $page)) {
+			$page = get_fullname(strip_bracket($page), $vars['page']);
+		} else {
+			return FALSE;
+		}
+	}
+	if (! is_page($page)) return FALSE;
+
+	return format_date(get_filetime($page));
+}
+?>
