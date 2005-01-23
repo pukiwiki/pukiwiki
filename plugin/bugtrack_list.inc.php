d75c32151374c4c0749d4e136eeeadfbff5809f2
henoheno <henoheno>
2005-01-23 16:20:19 +0900

    A little cleanup

--- plugin/bugtrack_list.inc.php
+++ plugin/bugtrack_list.inc.php
@@ -1,17 +1,14 @@
 <?php
+// $Id: bugtrack_list.inc.php,v 1.5 2005/01/23 07:20:19 henoheno Exp $
+//
+// BugTrack-list plugin - A part of BugTrack plugin
 /*
- * PukiWiki BugTrackプラグイン
- *
+ * PukiWiki BugTrack plugin
  * CopyRight 2002 Y.MASUI GPL2
  * http://masui.net/pukiwiki/ masui@masui.net
- *
- * 変更履歴:
- *  2002.06.17: 作り始め
- *
- * $Id: bugtrack_list.inc.php,v 1.4 2004/07/31 03:09:20 henoheno Exp $
  */
 
-require_once(PLUGIN_DIR.'bugtrack.inc.php');
+require_once(PLUGIN_DIR . 'bugtrack.inc.php');
 
 function plugin_bugtrack_list_init()
 {
