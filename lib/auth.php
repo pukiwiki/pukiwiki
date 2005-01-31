161dd68318298e0f20a4be388d00581de472ae5c
henoheno <henoheno>
2005-01-31 23:32:38 +0900

    PKWK_READONLY prohibits basic_auth

--- lib/auth.php
+++ lib/auth.php
@@ -1,6 +1,6 @@
 <?php
-// PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: auth.php,v 1.6 2004/12/31 10:51:47 henoheno Exp $
+// PukiWiki - Yet another WikiWikiWeb clone
+// $Id: auth.php,v 1.7 2005/01/31 14:32:38 henoheno Exp $
 //
 // Basic authentication related functions
 
@@ -87,7 +87,8 @@ function basic_auth($page, $auth_flag, $exit_flag, $auth_pages, $title_cannot)
 	}
 
 	// ユーザリストに含まれるいずれかのユーザと認証されればOK
-	if (! isset($_SERVER['PHP_AUTH_USER'])
+	if (PKWK_READONLY
+		|| ! isset($_SERVER['PHP_AUTH_USER'])
 		|| ! in_array($_SERVER['PHP_AUTH_USER'], $user_list)
 		|| ! isset($_SERVER['PHP_AUTH_USER'], $auth_users)
 		|| $auth_users[$_SERVER['PHP_AUTH_USER']] != $_SERVER['PHP_AUTH_PW'])
