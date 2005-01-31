80c0cc7fc3d3830eaf5efba2dd48fe2f1acb13fe
henoheno <henoheno>
2005-01-31 22:03:41 +0900

    BugTrack/602: Allow using BracketName with or without brackets (by Nibun-no-ni)

--- plugin/lastmod.inc.php
+++ plugin/lastmod.inc.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone
-// $Id: lastmod.inc.php,v 1.2 2005/01/23 13:57:38 henoheno Exp $
+// $Id: lastmod.inc.php,v 1.3 2005/01/31 13:03:41 henoheno Exp $
 //
 // Lastmod plugin - Show lastmodifled date of the page
 // Originally written by Reimy, 2003
@@ -15,7 +15,7 @@ function plugin_lastmod_inline()
 	if ($page == ''){
 		$page = $vars['page']; // Default: page itself
 	} else {
-		if (preg_match("/^($WikiName|\[\[$BracketName\]\])$/", $page)) {
+		if (preg_match("/^($WikiName|$BracketName)$/", strip_bracket($page))) {
 			$page = get_fullname(strip_bracket($page), $vars['page']);
 		} else {
 			return FALSE;
