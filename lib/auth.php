796410b5d7f0d70da9477bc11b34fced319ce3cc
henoheno <henoheno>
2005-03-27 19:49:26 +0900

    Cleanup. Japanese => English

--- lib/auth.php
+++ lib/auth.php
@@ -1,51 +1,51 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone
-// $Id: auth.php,v 1.8 2005/02/26 12:10:14 henoheno Exp $
+// $Id: auth.php,v 1.9 2005/03/27 10:49:26 henoheno Exp $
 //
 // Basic authentication related functions
 
-// 編集不可能なページを編集しようとしたとき
+// Check edit-permission
 function check_editable($page, $auth_flag = TRUE, $exit_flag = TRUE)
 {
 	global $script, $_title_cannotedit, $_msg_unfreeze;
 
-	if (edit_auth($page, $auth_flag, $exit_flag) && is_editable($page))
+	if (edit_auth($page, $auth_flag, $exit_flag) && is_editable($page)) {
+		// Editable
 		return TRUE;
-
-	if (! $exit_flag) return FALSE;
-
-	$body = $title = str_replace('$1', htmlspecialchars(strip_bracket($page)), $_title_cannotedit);
-
-	if (is_freeze($page))
-		$body .= '(<a href="' . $script . '?cmd=unfreeze&amp;page=' .
-			rawurlencode($page) . '">' . $_msg_unfreeze . '</a>)';
-
-	$page = str_replace('$1', make_search($page), $_title_cannotedit);
-
-	catbody($title, $page, $body);
-	exit;
+	} else {
+		// Not editable
+		if ($exit_flag === FALSE) {
+			return FALSE; // Without exit
+		} else {
+			// With exit
+			$body = $title = str_replace('$1',
+				htmlspecialchars(strip_bracket($page)), $_title_cannotedit);
+			if (is_freeze($page))
+				$body .= '(<a href="' . $script . '?cmd=unfreeze&amp;page=' .
+					rawurlencode($page) . '">' . $_msg_unfreeze . '</a>)';
+			$page = str_replace('$1', make_search($page), $_title_cannotedit);
+			catbody($title, $page, $body);
+			exit;
+		}
+	}
 }
 
-// 閲覧不可能なページを閲覧しようとしたとき (？)
+// Check read-permission
 function check_readable($page, $auth_flag = TRUE, $exit_flag = TRUE)
 {
 	return read_auth($page, $auth_flag, $exit_flag);
 }
 
-// 編集認証
 function edit_auth($page, $auth_flag = TRUE, $exit_flag = TRUE)
 {
 	global $edit_auth, $edit_auth_pages, $_title_cannotedit;
-
 	return $edit_auth ?  basic_auth($page, $auth_flag, $exit_flag,
 		$edit_auth_pages, $_title_cannotedit) : TRUE;
 }
 
-// 閲覧認証
 function read_auth($page, $auth_flag = TRUE, $exit_flag = TRUE)
 {
 	global $read_auth, $read_auth_pages, $_title_cannotread;
-
 	return $read_auth ?  basic_auth($page, $auth_flag, $exit_flag,
 		$read_auth_pages, $_title_cannotread) : TRUE;
 }
@@ -56,17 +56,14 @@ function basic_auth($page, $auth_flag, $exit_flag, $auth_pages, $title_cannot)
 	global $auth_users, $auth_method_type;
 	global $_msg_auth;
 
-	// 認証要否判断対象
+	// Checked by:
 	$target_str = '';
 	if ($auth_method_type == 'pagename') {
-		// ページ名でチェックする
-		$target_str = $page;
+		$target_str = $page; // Page name
 	} else if ($auth_method_type == 'contents') {
-		// ページ内の文字列でチェックする
-		$target_str = join('', get_source($page));
+		$target_str = join('', get_source($page)); // Its contents
 	}
 
-	// 合致したパターンで定義されたユーザのリスト
 	$user_list = array();
 	foreach($auth_pages as $key=>$val)
 		if (preg_match($key, $target_str))
@@ -74,24 +71,23 @@ function basic_auth($page, $auth_flag, $exit_flag, $auth_pages, $title_cannot)
 
 	if (empty($user_list)) return TRUE; // No limit
 
-	// PHP_AUTH* 変数が未定義の場合
 	$matches = array();
-	if (! isset($_SERVER['PHP_AUTH_USER'])
-		&& ! isset($_SERVER ['PHP_AUTH_PW'])
-		&& isset($_SERVER['HTTP_AUTHORIZATION'])
-		&& preg_match('/^Basic (.*)$/', $_SERVER['HTTP_AUTHORIZATION'], $matches))
+	if (! isset($_SERVER['PHP_AUTH_USER']) &&
+		! isset($_SERVER ['PHP_AUTH_PW']) &&
+		isset($_SERVER['HTTP_AUTHORIZATION']) &&
+		preg_match('/^Basic (.*)$/', $_SERVER['HTTP_AUTHORIZATION'], $matches))
 	{
-		// HTTP_AUTHORIZATION 変数を使用した Basic 認証
+
+		// Basic-auth with $_SERVER['HTTP_AUTHORIZATION']
 		list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) =
 			explode(':', base64_decode($matches[1]));
 	}
 
-	// ユーザリストに含まれるいずれかのユーザと認証されればOK
-	if (PKWK_READONLY
-		|| ! isset($_SERVER['PHP_AUTH_USER'])
-		|| ! in_array($_SERVER['PHP_AUTH_USER'], $user_list)
-		|| ! isset($auth_users[$_SERVER['PHP_AUTH_USER']])
-		|| $auth_users[$_SERVER['PHP_AUTH_USER']] != $_SERVER['PHP_AUTH_PW'])
+	if (PKWK_READONLY ||
+		! isset($_SERVER['PHP_AUTH_USER']) ||
+		! in_array($_SERVER['PHP_AUTH_USER'], $user_list) ||
+		! isset($auth_users[$_SERVER['PHP_AUTH_USER']]) ||
+		$auth_users[$_SERVER['PHP_AUTH_USER']] != $_SERVER['PHP_AUTH_PW'])
 	{
 		pkwk_common_headers();
 		if ($auth_flag) {
