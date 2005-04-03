5a054c4bc5bd6e4792dbd13ed4cc3c27b3c1b8ad
henoheno <henoheno>
2005-04-03 11:51:07 +0900

    Correct comments only

--- plugin/bugtrack_list.inc.php
+++ plugin/bugtrack_list.inc.php
@@ -1,12 +1,11 @@
 <?php
-// $Id: bugtrack_list.inc.php,v 1.5 2005/01/23 07:20:19 henoheno Exp $
+// $Id: bugtrack_list.inc.php,v 1.6 2005/04/03 02:51:07 henoheno Exp $
 //
-// BugTrack-list plugin - A part of BugTrack plugin
-/*
- * PukiWiki BugTrack plugin
- * CopyRight 2002 Y.MASUI GPL2
- * http://masui.net/pukiwiki/ masui@masui.net
- */
+// PukiWiki BugTrack-list plugin - A part of BugTrack plugin
+//
+// Copyright
+// 2002-2005 PukiWiki Developers Team
+// 2002 Y.MASUI GPL2 http://masui.net/pukiwiki/ masui@masui.net
 
 require_once(PLUGIN_DIR . 'bugtrack.inc.php');
 
