b5572f73874ddeec3aee52c76b80d8e5cf3b7608
henoheno <henoheno>
2005-05-02 11:46:23 +0900

    Simplify only

--- plugin/add.inc.php
+++ plugin/add.inc.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: add.inc.php,v 1.6 2005/01/23 05:30:27 henoheno Exp $
+// $Id: add.inc.php,v 1.7 2005/05/02 02:46:23 henoheno Exp $
 //
 // Add plugin - Append new text below/above existing page
 // Usage: cmd=add&page=pagename
@@ -15,8 +15,13 @@ function plugin_add_action()
 	check_editable($page);
 
 	$get['add'] = $post['add'] = $vars['add'] = TRUE;
-	return array('msg'=>$_title_add, 'body' => '<ul>' . "\n" .
-		' <li>' . $_msg_add . '</li>' . "\n" . '</ul>' . "\n" .
-		edit_form($page, ''));
+	return array(
+		'msg'  => $_title_add,
+		'body' =>
+			'<ul>' . "\n" .
+			' <li>' . $_msg_add . '</li>' . "\n" .
+			'</ul>' . "\n" .
+			edit_form($page, '')
+		);
 }
 ?>
