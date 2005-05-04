0ff0524d1f19488b91a19c4399e5e8f1dee36fff
henoheno <henoheno>
2005-05-04 14:00:31 +0900

    BugTrack/779: Cleanup. Added spaces. Shrink. Double quote => single quote. 'and' for boolean => '&&. $increment++ => ++$incremen

--- plugin/calendar_edit.inc.php
+++ plugin/calendar_edit.inc.php
@@ -1,27 +1,19 @@
 <?php
-/////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
-//
-// $Id: calendar_edit.inc.php,v 1.7 2004/07/31 03:09:20 henoheno Exp $
+// $Id: calendar_edit.inc.php,v 1.8 2005/05/04 05:00:31 henoheno Exp $
 //
 
 function plugin_calendar_edit_convert()
 {
 	global $command;
 
-	$command = 'edit';
+	if (! file_exists(PLUGIN_DIR . 'calendar.inc.php')) return FALSE;
 
-	if (!file_exists(PLUGIN_DIR.'calendar.inc.php'))
-	{
-		return FALSE;
-	}
-	require_once PLUGIN_DIR.'calendar.inc.php';
-	if (!function_exists('plugin_calendar_convert'))
-	{
-		return FALSE;
-	}
+	$command = 'edit';
+	require_once PLUGIN_DIR . 'calendar.inc.php';
+	if (! function_exists('plugin_calendar_convert')) return FALSE;
 
 	$args = func_num_args() ? func_get_args() : array();
-	return call_user_func_array('plugin_calendar_convert',$args);
+	return call_user_func_array('plugin_calendar_convert', $args);
 }
 ?>
