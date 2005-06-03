f736868157824efa04cfcbc1c1636da1dd741947
henoheno <henoheno>
2005-06-03 22:56:25 +0900

    BugTrack/709: Re-added some $adminpass examples, to suggest 'what can do' to administrators

--- pukiwiki.ini.php
+++ pukiwiki.ini.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone
-// $Id: pukiwiki.ini.php,v 1.124 2005/05/28 13:31:56 henoheno Exp $
+// $Id: pukiwiki.ini.php,v 1.125 2005/06/03 13:56:25 henoheno Exp $
 // Copyright (C)
 //   2002-2005 PukiWiki Developers Team
 //   2001-2002 Originally written by yu-ji
@@ -177,6 +177,9 @@ $notimeupdate = 1;
 
 // CHANGE THIS
 $adminpass = '{x-php-md5}1a1dc91c907325c69271ddf0c944bc72'; // md5('pass')
+//$adminpass = '{CRYPT}$1$AR.Gk94x$uCe8fUUGMfxAPH83psCZG/'; // CRYPT 'pass'
+//$adminpass = '{MD5}Gh3JHJBzJcaScd3wyUS8cg==';             // MD5   'pass'
+//$adminpass = '{SMD5}o7lTdtHFJDqxFOVX09C8QnlmYmZnd2Qx';    // SMD5  'pass'
 
 /////////////////////////////////////////////////
 // Page-reading feature settings
