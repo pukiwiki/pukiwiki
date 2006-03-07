f2ff7f7647c3ebf8ac8f32d00ab34da0907036fa
henoheno <henoheno>
2006-03-07 23:27:23 +0900

    Added special definitions templates. Corrected copyright 2004-2005 => 2001-2006

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: index.php,v 1.7 2005/10/02 15:28:47 henoheno Exp $
-// Copyright (C) 2004-2005 PukiWiki Developers Team
+// $Id: index.php,v 1.8 2006/03/07 14:27:23 henoheno Exp $
+// Copyright (C) 2001-2006 PukiWiki Developers Team
 // License: GPL v2 or (at your option) any later version
 
-/////////////////////////////////////////////////
 // Error reporting
-
-// error_reporting(0); // Nothing
+//error_reporting(0); // Nothing
 error_reporting(E_ERROR | E_PARSE); // Avoid E_WARNING, E_NOTICE, etc
-// error_reporting(E_ALL);
+//error_reporting(E_ALL); // Debug purpose
 
-/////////////////////////////////////////////////
+// Special
+//define('PKWK_READONLY', 1);
+//define('PKWK_SAFE_MODE', 1);
+//define('PKWK_OPTIMISE', 1);
+//define('TDIARY_THEME', 'digital_gadgets');
+
 // Directory definition
 // (Ended with a slash like '../path/to/pkwk/', or '')
 define('DATA_HOME',	'');
 define('LIB_DIR',	'lib/');
 
-/////////////////////////////////////////////////
 require(LIB_DIR . 'pukiwiki.php');
 ?>
