e9f232e75fd47985ebb94e229796008593376776
teanan <teanan>
2006-04-16 03:02:38 +0900

    BugTrack2/171: The filetime is not needed when $non_format is true.

--- lib/func.php
+++ lib/func.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: func.php,v 1.59 2006/04/15 17:33:35 teanan Exp $
+// $Id: func.php,v 1.60 2006/04/15 18:02:38 teanan Exp $
 // Copyright (C)
 //   2002-2006 PukiWiki Developers Team
 //   2001-2002 Originally written by yu-ji
@@ -242,7 +242,7 @@ function do_search($word, $type = 'AND', $non_format = FALSE, $base = '')
 			$b_match = preg_match('/' . $key . '/S', $source);
 			if ($b_match xor $b_type) break;
 		}
-		if ($b_match) $pages[$page] = get_filetime($page);
+		if ($b_match) $pages[$page] = $non_format ? 0 : get_filetime($page);
 	}
 	if ($non_format) return array_keys($pages);
 
