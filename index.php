c85e417bcc8febb9b53f992c0170d501010b49a7
henoheno <henoheno>
2006-05-13 16:39:49 +0900

    Added spaces only

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: index.php,v 1.8 2006/03/07 14:27:23 henoheno Exp $
+// $Id: index.php,v 1.9 2006/05/13 07:39:49 henoheno Exp $
 // Copyright (C) 2001-2006 PukiWiki Developers Team
 // License: GPL v2 or (at your option) any later version
 
@@ -10,10 +10,10 @@ error_reporting(E_ERROR | E_PARSE); // Avoid E_WARNING, E_NOTICE, etc
 //error_reporting(E_ALL); // Debug purpose
 
 // Special
-//define('PKWK_READONLY', 1);
+//define('PKWK_READONLY',  1);
 //define('PKWK_SAFE_MODE', 1);
-//define('PKWK_OPTIMISE', 1);
-//define('TDIARY_THEME', 'digital_gadgets');
+//define('PKWK_OPTIMISE',  1);
+//define('TDIARY_THEME',   'digital_gadgets');
 
 // Directory definition
 // (Ended with a slash like '../path/to/pkwk/', or '')
