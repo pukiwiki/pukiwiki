cad1a62f419fb6b8fd887f9e8c8f2fde8479b1f1
henoheno <henoheno>
2006-12-25 00:17:39 +0900

    Existing URI redirection or masking services, as known as cheap URI obscuring services today, for spammers and affiliate users dazed by money.

---
+++ spam.ini.php
@@ -0,0 +1,153 @@
+<?php
+// $Id: spam.ini.php,v 1.1 2006/12/24 15:17:39 henoheno Exp $
+// Spam-related setting
+
+$blocklist['badhost'] = array(
+	//'*',	// Simply deny ALL uri
+	//'10.20.*.*',	// 10.20.example.com also matches
+
+	// Existing URI redirection or masking services,
+	// as known as cheap URI obscuring services today,
+	// for spammers and affiliate users dazed by money.
+	'*.0kn.com',	// by shim.net
+	'0rz.tw',
+	'0url.com',
+	'*.1sta.com',	// by shorturl.com
+	'*.24ex.com',	// by shorturl.com
+	'*.2cd.net',	// by shim.net
+	'*.2fear.com',	// by shorturl.com
+	'*.2fortune.com',	// by shorturl.com
+	'*.2freedom.com',	// by shorturl.com
+	'*.2hell.com',	// by shorturl.com
+	'2hop4.com',
+	'*.2savvy.com',	// by shorturl.com
+	'*.2truth.com',	// by shorturl.com
+	'*.2tunes.com',	// by shorturl.com
+	'*.2ya.com',	// by shorturl.com
+	'301url.com',
+	'*.321.cn',		// by active.ws
+	'*.4bb.ru',
+	'*.4mg.com',	// by freeservers.com
+	'*.4x2.net',	// by active.ws
+	'5jp.net',
+	'*.6url.com',
+	'82m.org',
+	'active.ws',
+	'*.alturl.com',	// by shorturl.com
+	'*.antiblog.com',	// by shorturl.com
+	'ataja.es',
+	'*.better.ws',	// by active.ws
+	'*.bigbig.com',	// by shorturl.com
+	'bingr.com',
+	'brokenscript.com',
+	'clipurl.com',
+	'*.dealtap.com',	// by shorturl.com
+	'dephine.org',
+	'digbig.com',
+	'*.digipills.com',
+	'doiop.com',
+	'*.ebored.com',	// by shorturl.com
+	'*.echoz.com',	// by shorturl.com
+	'elfurl.com',
+	'*.f2b.be',		// by f2b.be
+	'*.fancyurl.com',
+	'ffwd.to',
+	'*.filetap.com',	// by shorturl.com
+	'flingk.com',
+	'*.freakz.eu',	// by f2b.be
+	'*.freebiefinders.net',	// by shim.net
+	'*.freegaming.org',	// by shim.net
+	'*.funurl.com',	// by shorturl.com
+	'gentleurl.net',
+	'*.headplug.com',	// by shorturl.com
+	'*.here.ws',	// by active.ws
+	'*.hereweb.com',	// by shorturl.com
+	'*.hitart.com',	// by shorturl.com
+	'*.hux.de',
+	'*.i89.us',
+	'*.iceglow.com',
+	'igoto.co.uk',
+	'iscool.net',
+	'kat.cc',
+	'lame.name',
+	'linkezy.com',
+	'lnk.in',
+	'makeashorterlink.com',
+	'memurl.com',
+	'minilien.com',		// by digipills.com
+	'*.minilien.com',	// by digipills.com
+	'*.mirrorz.com',	// by shorturl.com
+	'mo-v.jp',
+	'*.mypiece.com',// by active.ws
+	'mytinylink.com',
+	'myurl.in',
+	'*.n0.be',		// by f2b.be
+	'*.n3t.nl',		// by f2b.be
+	'*.ne1.net',
+	'*.notlong.com',
+	'*.official.ws',// by active.ws
+	'*.op7.net',	// by shim.net
+	'*.ouch.ws',	// by active.ws
+	'*.paulding.net',
+	'pnope.com',
+	'*.premium.ws',	// by active.ws
+	'qrl.jp',
+	'qurl.net',
+	'*.r8.org',	// by ne1.net
+	's-url.net',
+	'*.sg5.info',
+	'*.shim.net',
+	'shorl.com',
+	'*.short.be',	// by f2b.be
+	'shortenurl.com',
+	'shorterlink.com',
+	'shortlinks.co.uk',
+	'shorttext.com',
+	'*.shorturl.com',
+	'shrinkurl.us',
+	'shrunkurl.com',
+	'shurl.org',
+	'shurl.net',
+	'simurl.com',
+	'skocz.pl',
+	'snipurl.com',
+	'*.spyw.com',	// by shorturl.com
+	'*.ssr.be',		// by f2b.be
+	'*.such.info',	// by active.ws
+	'surl.dk',		// by s-url.dk
+	'*.tiny.cc',
+	'tiny.pl',
+	'tiny2go.com',
+	'tinylink.com',	// by digipills.com
+	'tinypic.com',
+	'tinyr.us',
+	'tinyurl.com',
+	'*.toolbot.com',
+	'*.torontonian.com',
+	'*.true.ws',	// by active.ws
+	'*.tweaker.eu',	// by f2b.be
+	'*.tz4.com',
+	'*.uploadr.com',
+	'url.vg',
+	'url4.net',
+	'*.url4.net',
+	'urlcut.com',
+	'urlcutter.com',
+	'urlic.com',
+	'urlsnip.com',
+	'urlzip.de',
+	'urlx.org',
+	'*.v9z.com',	// by shim.net
+	'*.visit.ws',	// by active.ws
+	'*.vze.com',	// by shorturl.com
+	'wiz.sc',	// tiny.cc related
+	'xrl.us',	// by metamark.net
+	'y11.net',
+	'yep.it',
+	'z.la',
+	't.z.la',	// by z.la
+	'zapurl.com',
+	'zippedurl.com',
+);
+
+?>
