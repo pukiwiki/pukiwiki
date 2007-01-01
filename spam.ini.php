eee402c6e7bc28bdde203398db3d44c7e78b5bcf
henoheno <henoheno>
2007-01-01 23:33:29 +0900

    * Added more sample of redirections * Added sample setting of: Jacked sites * Added sample setting of: Exclusive spam domains

--- spam.ini.php
+++ spam.ini.php
@@ -1,9 +1,10 @@
 <?php
-// $Id: spam.ini.php,v 1.3 2006/12/30 09:26:59 henoheno Exp $
+// $Id: spam.ini.php,v 1.4 2007/01/01 14:33:29 henoheno Exp $
 // Spam-related setting
 
 $blocklist['badhost'] = array(
 
+	// Sample setting of:
 	// Existing URI redirection or masking services,
 	// as known as cheap URI obscuring services today,
 	// for spammers and affiliate users dazed by money.
@@ -52,6 +53,7 @@ $blocklist['badhost'] = array(
 	'*.8k.com',			// by freeservers.com
 	'*.abwb.org',
 	'active.ws',
+	'store.adobe.com',	// Stop it
 	'*.alturl.com',		// by shorturl.com
 	'*.andmuchmore.com',// by webalias.com
 	'*.antiblog.com',	// by shorturl.com
@@ -70,6 +72,7 @@ $blocklist['badhost'] = array(
 	'*.buzznet.com',
 	'*.c0m.st',			// by ulimit.com
 	'*.ca.tc',			// by ulimit.com
+	'*.chicappa.jp',
 	'*.clan.st',		// by ulimit.com
 	'clipurl.com',
 	'*.com02.com',		// by ulimit.com
@@ -147,6 +150,7 @@ $blocklist['badhost'] = array(
 	'*.latest-info.com',// by webalias.com
 	'*.learn.to',		// by webalias.com
 	'linkezy.com',
+	'linkook.com',
 	'*.linux-fan.com',	// by ulimit.com
 	'lnk.in',
 	'*.mac-fan.com',	// by ulimit.com
@@ -156,6 +160,7 @@ $blocklist['badhost'] = array(
 	'*.minilien.com',	// by digipills.com
 	'*.mirrorz.com',	// by shorturl.com
 	'mo-v.jp',
+	'mooo.jp',
 	'*.moviefever.com',	// by webalias.com
 	'*.mp3.ms',			// by ulimit.com
 	'*.mp3-archives.com',	// by webalias.com
@@ -281,6 +286,181 @@ $blocklist['badhost'] = array(
 	'*.zik.mu',			// by ulimit.com
 	'zippedurl.com',
 	'*.zonehere.com',	// by hotredirect.com
+
+
+	// Sample setting of: Jacked sites (taken advantage of)
+	//
+	// Please notify us about this list with reason:
+	// http://pukiwiki.sourceforge.jp/dev/?BugTrack2%2F208
+
+	// 1. Web-spaces especially taken advantage of
+	'*.0catch.com',
+	'*.20six.nl',
+	'*.9999mb.com',
+	'*.alice.it',
+	'*.alkablog.com'.
+	'*.atfreeforum.com',
+	'*.asphost4free.com',
+	'bloggers.nl',
+	'*.bloggers.nl',
+	'*.blogspot.com',
+	'*.bravenet.com',
+	'*.free-25.de',
+	'*.freelinuxhost.com',	// by 100webspace.com
+	'groups-beta.google.com',
+	'hometown.aol.com',
+	'*.journalscape.com',
+	'myblog.de',
+	'myblog.es',
+	'*.myblogvoice.com',
+	'*.nm.ru',
+	'*.quickfreehost.com',
+	'*.sbn.bz',				// by rin.ru
+	'*.squarespace.com',
+	'*.t35.com',
+	'*.welover.org',
+	'*.weblogmaniacs.com',
+	'weblogmaniacs.com',
+	'*.wol.bz', // by sbn.bz (rin.ru)
+
+	// 2. (Seems to be) Jacked contents, something implanted
+	// (e.g. some sort of blog comments, BBSes, forums, wikis)
+	'*.aamad.org',
+	'www.blepharospasm.org',
+	'*.colourware.co.uk',
+	'*.iphpbb.com',
+	'board-z.de',
+	'*.board-z.de',
+	'*.fhmcsa.org.au',
+	'forum.lixium.fr',
+	'funkdoc.com',
+	'www.homepage-dienste.com',
+	'www.macfaq.net',
+	'www.me4x4.com',
+	'rkphunt.com',
+	'www.saskchamber.com',
+	'selikoff.net',
+	'www.tzaneen.co.za',
+
+
+	// Sample setting of: Exclusive spam domains
+	// seems to have flavor of links, pills, gamble, erotic,
+	// affiliates, and/or mixed ones
+	//
+	// Please notify us about this list with reason:
+	// http://pukiwiki.sourceforge.jp/dev/?BugTrack2/208
+
+	// 1. Domain sets (seems to be) born to spam you
+	'*.lovestoryx.com',	// by Boris (seekforweb.com, bbmfree at yahoo.com)
+	'*.loveaffairx.com',// by Boris (seekforweb.com, bbmfree at yahoo.com)
+	'*.onmore.info',	// by Boris (seekforweb.com, bbmfree at yahoo.com)
+	'*.scfind.info',	// by Boris (seekforweb.com, bbmfree at yahoo.com)
+	'*.webwork88.info',	// by Boris (seekforweb.com, bbmfree at yahoo.com)
+	//
+	'htewbop.org',		// by Boris (boss at bse-sofia.bg)
+	'*.htewbop.org',	// by Boris (boss at bse-sofia.bg)
+	'*.kimm--d.org',	// by Boris (boss at bse-sofia.bg)
+	'*.gtre--h.org',	// by Boris (boss at bse-sofia.bg)
+	'*.npou--k.org',	// by Boris (boss at bse-sofia.bg)
+	'*.bres--z.org',	// by Boris (boss at bse-sofia.bg)
+	'*.berk--p.org',	// by Boris (boss at bse-sofia.bg)
+	'*.bplo--s.org',	// by Boris (boss at bse-sofia.bg)
+	'*.basdpo.org',		// by Boris (boss at bse-sofia.bg)
+	'*.mertnop.org',	// by Boris (boss at bse-sofia.bg)
+	'vasdipv.org',		// by Boris (boss at bse-sofia.bg)
+	'*.vasdipv.org',	// by Boris (boss at bse-sofia.bg)
+	//
+	'*.axa00.info',		// by Thai Dong Changli (pokurim at gamebox.net)
+	'*.okweb11.org',	// by Thai Dong Changli (pokurim at gamebox.net)
+	//
+	'informazionicentro.info',	// by opezdol at gmail.com
+	'*.informazionicentro.info',// by opezdol at gmail.com
+	'notiziacentro.info',		// by opezdol at gmail.com
+	'*.notiziacentro.info',		// by opezdol at gmail.com
+	//
+	'*.adult-chat-world.info',	// by CamsGen (camsgen at model-x.com)
+	'*.adult-sex-chat.info',	// by CamsGen (camsgen at model-x.com)
+	'*.camshost.info',			// by CamsGen (buckster at hotpop.com)
+	'*.camdoors.info',			// by CamsGen (buckster at hotpop.com)
+	'*.chatdoors.info',			// by CamsGen (buckster at hotpop.com)
+	'*.dildo-chat.org',			// by CamsGen (camsgen at model-x.com)
+	'*.live-adult-chat.org',	// by CamsGen (camsgen at model-x.com)
+	'*.sexy-chat-rooms.org',	// by CamsGen (camsgen at model-x.com)
+	'*.swinger-sex-chat.org',	// by CamsGen (camsgen at model-x.com)
+	'*.nasty-sex-chat.org',		// by CamsGen (camsgen at model-x.com)
+	'*.flirt-online.org',		// by CamsGen (camsgen at model-x.com)
+	//
+	'*.trevisos.org',	// by Marcello Italianore (mital at topo20.org)
+	'*.topo20.org',		// by Marcello Italianore (mital at topo20.org)
+	//
+	'*.wellcams.com',
+	'*.j8v9.info',		// by wellcams.com
+	'wellcams.biz',		// by wellcams.com
+	//
+	'*.besturgent.org',		// by Chinu Hua Dzin (graz at rubli.biz)
+	'*.googletalknow.org',	// by Chinu Hua Dzin (graz at rubli.biz)
+	'*.montypythonltd.org',	// by Chinu Hua Dzin (graz at rubli.biz)
+	'*.supersettlemet.org',	// by Chinu Hua Dzin (graz at rubli.biz)
+	'*.thepythonfoxy.org',	// by Chinu Hua Dzin (graz at rubli.biz)
+	'*.ukgamesyahoo.org',	// by Chinu Hua Dzin (graz at rubli.biz)
+	'*.youryahoochat.org',	// by Chinu Hua Dzin (graz at rubli.biz)
+	//
+	'*.casinoqz.com',
+	'*.dcasinoa.com',		// would be casinoqz.com
+	//
+	'*.kenogo.com',
+	'*.mycaribbeanpoker.com',	// would be kenogo.com
+	//
+	'*.koosx.org',		// by Kikimas at mail.net, Redirect to nb717.com etc
+	'*.mmgz.org',		// by Kikimas at mail.net, Redirect to nb717.com etc
+	//
+	'43sexx.org',		// by Andrey (vdf at lovespb.com)
+	'56porn.org',		// by Andrey (vdf at lovespb.com)
+	//
+	'*.flywebs.com',	// by Andrey Zhurikov (zhu1313 at mail.ru)
+	'*.hostrim.com',	// by Andrey Zhurikov (zhu1313 at mail.ru)
+	'playbit.com',		// by Andrey Zhurikov (zhu1313 at mail.ru)
+
+	// 2. Lonely domains (buddies not found yet)
+	'*.aimoo.com',
+	'anewme.org',
+	'*.areaseo.com',
+	'*.bsb3b.info',
+	'*.buzznet.com',
+	'daintyurl.com',
+	'*.dgo5d.info',
+	'diabetescarelink.com',
+	'*.dlekei.info',
+	'*.discutbb.com',
+	'fingerprintmedia.com',
+	'firstdebthelp.com',
+	'hotscriptonline.info',
+	'*.hut1.ru',
+	'implex3.com',
+	'italy-search.org',
+	'*.italy-search.org',
+	'*.infogami.com',
+	'*.mujiki.com',
+	'*.okweb12.org',
+	'*.pahuist.info',
+	'*.perevozka777.ru',
+	'*.pokah.lv',
+	'portaldiscount.com',
+	'*.portaldiscount.com',
+	'qoclick.net',
+	'relurl.com',		// tiny-like
+	'*.scinfo.info',
+	'*.selab.org.ua',
+	'sirlook.com',
+	'*.soft2you.info',
+	'softprof.org'
+	'*.stormloader.com',
+	'tops.gen.in',		// Hiding google:sites
+	'vasdipv.org',
+	'wapurl.co.uk',
+	'*.webnow.biz',
+	'wellcams.biz',
+	'wwwtahoo.com',		// Typo:yahoo.com
 
 );
 ?>
