7579000c8c18cc5321c7450ec23a67935f6e785f
henoheno <henoheno>
2007-05-17 23:11:44 +0900

    Simplify

--- plugin/recent.inc.php
+++ plugin/recent.inc.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: recent.inc.php,v 1.25 2007/05/17 13:35:49 henoheno Exp $
+// $Id: recent.inc.php,v 1.26 2007/05/17 14:11:44 henoheno Exp $
 // Copyright (C)
 //   2002-2007 PukiWiki Developers Team
 //   2002      Y.MASUI http://masui.net/pukiwiki/ masui@masui.net
@@ -28,60 +28,62 @@ function plugin_recent_convert()
 	static $exec_count = 1;
 
 	$recent_lines = PLUGIN_RECENT_DEFAULT_LINES;
-	if (func_num_args()) {
-		$args = func_get_args();
-		if (! is_numeric($args[0]) || isset($args[1])) {
+	$args = func_get_args();
+	if (! empty($args)) {
+		if (isset($args[1]) || ! is_numeric($args[0])) {
 			return PLUGIN_RECENT_USAGE . '<br />';
-		} else {
-			$recent_lines = $args[0];
 		}
+		$recent_lines = & $args[0];
 	}
 
-	// Show only N times
-	if ($exec_count > PLUGIN_RECENT_EXEC_LIMIT) {
-		return '#recent(): You called me too much' . '<br />' . "\n";
-	} else {
-		++$exec_count;
-	}
-
-	if (! file_exists(PLUGIN_RECENT_CACHE))
+	if ($exec_count++ > PLUGIN_RECENT_EXEC_LIMIT) {
+		return '#recent(): You called me too much' . '<br />';
+	} else if (! file_exists(PLUGIN_RECENT_CACHE)) {
 		return '#recent(): Cache file of RecentChanges not found' . '<br />';
+	}
 
-	// Get latest N changes
 	$lines = file_head(PLUGIN_RECENT_CACHE, $recent_lines);
-	if ($lines == FALSE) return '#recent(): File can not open' . '<br />' . "\n";
+	if ($lines == FALSE) {
+		return '#recent(): File can not open' . '<br />';
+	}
 
 	$script = get_script_uri();
-	$date = $items = '';
+	$date = '';
+	$items = array();
 	foreach ($lines as $line) {
 		list($time, $page) = explode("\t", rtrim($line));
 
 		$_date = get_date($date_format, $time);
 		if ($date != $_date) {
 			// End of the day
-			if ($date != '') $items .= '</ul>' . "\n";
+			if ($date != '') $items[] = '</ul>';
 
 			// New day
 			$date = $_date;
-			$items .= '<strong>' . $date . '</strong>' . "\n" .
-				'<ul class="recent_list">' . "\n";
+			$items[] = '<strong>' . $date . '</strong>';
+			$items[] = '<ul class="recent_list">';
 		}
 
 		$s_page = htmlspecialchars($page);
 
+		// "===" for two numerical strings
 		if ($page === $vars['page']) {
-			// No need to link to the page you just read, or notify where you just read
-			$items .= ' <li>' . $s_page . '</li>' . "\n";
+			// No need to link to the page itself.
+			// This also notify where you just read.
+			$items[] = ' <li>' . $s_page . '</li>';
 		} else {
-			$r_page = rawurlencode($page);
-			$passage = $show_passage ? ' ' . get_passage($time) : '';
-			$items .= ' <li><a href="' . $script . '?' . $r_page . '"' . 
-				' title="' . $s_page . $passage . '">' . $s_page . '</a></li>' . "\n";
+			$passage = $show_passage ? ' ' . htmlspecialchars(get_passage($time)) : '';
+			$items[] = ' <li><a href="' . $script . '?' . rawurlencode($page) . '"' . 
+				' title="' . $s_page . $passage . '">' . $s_page . '</a></li>';
 		}
 	}
-	// End of the day
-	if ($date != '') $items .= '</ul>' . "\n";
 
-	return sprintf($_recent_plugin_frame, count($lines), $items);
+	// End of the day
+	if ($date != '') $items[] = '</ul>';
+	
+	// Last "\n"
+	$items[] = '';
+
+	return sprintf($_recent_plugin_frame, count($lines), implode("\n", $items));
 }
 ?>
