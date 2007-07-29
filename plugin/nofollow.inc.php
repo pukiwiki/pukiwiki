4378dfeafc4b04349fb8ae6c3f4c46189bea2303
henoheno <henoheno>
2007-07-29 14:22:36 +0900

    Show a message when the page seems not freezed

--- plugin/nofollow.inc.php
+++ plugin/nofollow.inc.php
@@ -1,9 +1,10 @@
 <?php
-// $Id: nofollow.inc.php,v 1.1 2005/05/23 14:22:30 henoheno Exp $
-// Copyright (C) 2005 PukiWiki Developers Team
+// $Id: nofollow.inc.php,v 1.2 2007/07/29 05:22:36 henoheno Exp $
+// Copyright (C) 2005, 2007 PukiWiki Developers Team
 // License: The same as PukiWiki
 //
 // NoFollow plugin
+// (See BugTrack2/72)
 
 // Output contents with "nofollow,noindex" option
 function plugin_nofollow_convert()
@@ -11,9 +12,11 @@ function plugin_nofollow_convert()
 	global $vars, $nofollow;
 
 	$page = isset($vars['page']) ? $vars['page'] : '';
-
-	if(is_freeze($page)) $nofollow = 1;
-
-	return '';
+	if (is_freeze($page)) {
+		$nofollow = 1;
+		return '';
+	} else {
+		return '#nofollow: Page not freezed';
+	}
 }
 ?>
