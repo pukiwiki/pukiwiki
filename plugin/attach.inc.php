2b92bec1fe17dc0c690cc66df6f7d5ed64f9c60b
henoheno <henoheno>
2007-07-30 23:30:14 +0900

    BugTrack2/264: Compute MD5 if needed, not everytime! (patched by ioio & ryu1)

--- plugin/attach.inc.php
+++ plugin/attach.inc.php
@@ -449,7 +449,11 @@ class AttachFile
 		$this->logname  = $this->basename . '.log';
 		$this->exist    = file_exists($this->filename);
 		$this->time     = $this->exist ? filemtime($this->filename) - LOCALZONE : 0;
-		$this->md5hash  = $this->exist ? md5_file($this->filename) : '';
+	}
+
+	function gethash()
+	{
+		return $this->exist ? md5_file($this->filename) : '';
 	}
 
 	// ファイル情報取得
@@ -563,6 +567,7 @@ class AttachFile
 			}
 		}
 		$info = $this->toString(TRUE, FALSE);
+		$hash = $this->gethash();
 
 		$retval = array('msg'=>sprintf($_attach_messages['msg_info'], htmlsc($this->file)));
 		$retval['body'] = <<< EOD
@@ -574,7 +579,7 @@ class AttachFile
  <dt>$info</dt>
  <dd>{$_attach_messages['msg_page']}:$s_page</dd>
  <dd>{$_attach_messages['msg_filename']}:{$this->filename}</dd>
- <dd>{$_attach_messages['msg_md5hash']}:{$this->md5hash}</dd>
+ <dd>{$_attach_messages['msg_md5hash']}:$hash</dd>
  <dd>{$_attach_messages['msg_filesize']}:{$this->size_str} ({$this->size} bytes)</dd>
  <dd>Content-type:{$this->type}</dd>
  <dd>{$_attach_messages['msg_date']}:{$this->time_str}</dd>
