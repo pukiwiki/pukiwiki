56c5d2d7bacd17b1b5d661642b9ace6203654386
henoheno <henoheno>
2007-08-19 21:38:25 +0900

    Use $post to prohibit GET method

--- plugin/edit.inc.php
+++ plugin/edit.inc.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: edit.inc.php,v 1.41 2007/02/11 05:53:31 henoheno Exp $
+// $Id: edit.inc.php,v 1.42 2007/08/19 12:38:25 henoheno Exp $
 // Copyright (C) 2001-2006 PukiWiki Developers Team
 // License: GPL v2 or (at your option) any later version
 //
@@ -11,7 +11,7 @@ define('PLUGIN_EDIT_FREEZE_REGEX', '/^(?:#freeze(?!\w)\s*)+/im');
 
 function plugin_edit_action()
 {
-	global $vars, $_title_edit, $load_template_func;
+	global $post, $vars, $_title_edit, $load_template_func;
 
 	if (PKWK_READONLY) die_message('PKWK_READONLY prohibits editing');
 
@@ -19,11 +19,11 @@ function plugin_edit_action()
 
 	check_editable($page, true, true);
 
-	if (isset($vars['preview']) || ($load_template_func && isset($vars['template']))) {
+	if (isset($post['preview']) || ($load_template_func && isset($post['template']))) {
 		return plugin_edit_preview();
-	} else if (isset($vars['write'])) {
+	} else if (isset($post['write'])) {
 		return plugin_edit_write();
-	} else if (isset($vars['cancel'])) {
+	} else if (isset($post['cancel'])) {
 		return plugin_edit_cancel();
 	}
 
@@ -36,25 +36,25 @@ function plugin_edit_action()
 // Preview
 function plugin_edit_preview()
 {
-	global $vars;
-	global $_title_preview, $_msg_preview, $_msg_preview_delete;
+	global $post, $vars, $_title_preview, $_msg_preview, $_msg_preview_delete;
 
 	$page = isset($vars['page']) ? $vars['page'] : '';
 
 	// Loading template
-	if (isset($vars['template_page']) && is_page($vars['template_page'])) {
+	if (isset($post['template_page']) && is_page($post['template_page'])) {
 
-		$vars['msg'] = join('', get_source($vars['template_page']));
+		$post['msg'] = join('', get_source($post['template_page']));
 
 		// Cut fixed anchors
-		$vars['msg'] = preg_replace('/^(\*{1,3}.*)\[#[A-Za-z][\w-]+\](.*)$/m', '$1$2', $vars['msg']);
+		$post['msg'] = preg_replace('/^(\*{1,3}.*)\[#[A-Za-z][\w-]+\](.*)$/m', '$1$2', $post['msg']);
 	}
 
-	$vars['msg'] = preg_replace(PLUGIN_EDIT_FREEZE_REGEX, '', $vars['msg']);
-	$postdata = $vars['msg'];
+	$post['msg'] = preg_replace(PLUGIN_EDIT_FREEZE_REGEX, '', $post['msg']);
+	$postdata = $post['msg'];
 
+	// Compat: add plugin and adding contents
 	if (isset($vars['add']) && $vars['add']) {
-		if (isset($vars['add_top']) && $vars['add_top']) {
+		if (isset($post['add_top']) && $post['add_top']) {
 			$postdata  = $postdata . "\n\n" . @join('', get_source($page));
 		} else {
 			$postdata  = @join('', get_source($page)) . "\n\n" . $postdata;
@@ -72,7 +72,7 @@ function plugin_edit_preview()
 		$postdata = drop_submit(convert_html($postdata));
 		$body .= '<div id="preview">' . $postdata . '</div>' . "\n";
 	}
-	$body .= edit_form($page, $vars['msg'], $vars['digest'], FALSE);
+	$body .= edit_form($page, $post['msg'], $post['digest'], FALSE);
 
 	return array('msg'=>$_title_preview, 'body'=>$body);
 }
@@ -174,16 +174,16 @@ function plugin_edit_inline()
 // Write, add, or insert new comment
 function plugin_edit_write()
 {
-	global $vars;
+	global $post, $vars;
 	global $_title_collided, $_msg_collided_auto, $_msg_collided, $_title_deleted;
 	global $notimeupdate, $_msg_invalidpass, $do_update_diff_table;
 
 	$page   = isset($vars['page'])   ? $vars['page']   : '';
 	$add    = isset($vars['add'])    ? $vars['add']    : '';
-	$digest = isset($vars['digest']) ? $vars['digest'] : '';
+	$digest = isset($post['digest']) ? $post['digest'] : '';
 
-	$vars['msg'] = preg_replace(PLUGIN_EDIT_FREEZE_REGEX, '', $vars['msg']);
-	$msg = & $vars['msg']; // Reference
+	$post['msg'] = preg_replace(PLUGIN_EDIT_FREEZE_REGEX, '', $post['msg']);
+	$msg = & $post['msg'];
 
 	$retvars = array();
 
@@ -191,9 +191,9 @@ function plugin_edit_write()
 	$oldpagesrc = join('', get_source($page));
 	$oldpagemd5 = md5($oldpagesrc);
 	if ($digest != $oldpagemd5) {
-		$vars['digest'] = $oldpagemd5; // Reset
+		$post['digest'] = $oldpagemd5; // Reset
 
-		$original = isset($vars['original']) ? $vars['original'] : '';
+		$original = isset($post['original']) ? $post['original'] : '';
 		list($postdata_input, $auto) = do_update_diff($oldpagesrc, $msg, $original);
 
 		$retvars['msg' ] = $_title_collided;
@@ -205,15 +205,15 @@ function plugin_edit_write()
 
 	// Action?
 	if ($add) {
-		// Add
-		if (isset($vars['add_top']) && $vars['add_top']) {
+		// Compat: add plugin and adding contents
+		if (isset($post['add_top']) && $post['add_top']) {
 			$postdata  = $msg . "\n\n" . @join('', get_source($page));
 		} else {
 			$postdata  = @join('', get_source($page)) . "\n\n" . $msg;
 		}
 	} else {
 		// Edit or Remove
-		$postdata = & $msg; // Reference
+		$postdata = & $msg;
 	}
 
 	// NULL POSTING, OR removing existing page
@@ -225,8 +225,8 @@ function plugin_edit_write()
 	}
 
 	// $notimeupdate: Checkbox 'Do not change timestamp'
-	$notimestamp = isset($vars['notimestamp']) && $vars['notimestamp'] != '';
-	if ($notimeupdate > 1 && $notimestamp && ! pkwk_login($vars['pass'])) {
+	$notimestamp = isset($post['notimestamp']) && $post['notimestamp'] != '';
+	if ($notimeupdate > 1 && $notimestamp && ! pkwk_login($post['pass'])) {
 		// Enable only administrator & password error
 		$retvars['body']  = '<p><strong>' . $_msg_invalidpass . '</strong></p>' . "\n";
 		$retvars['body'] .= edit_form($page, $msg, $digest, FALSE);
@@ -234,6 +234,7 @@ function plugin_edit_write()
 	}
 
 	page_write($page, $postdata, $notimeupdate != 0 && $notimestamp);
+
 	pkwk_headers_sent();
 	header('Location: ' . get_script_uri() . '?' . rawurlencode($page));
 	exit;
@@ -243,6 +244,7 @@ function plugin_edit_write()
 function plugin_edit_cancel()
 {
 	global $vars;
+
 	pkwk_headers_sent();
 	header('Location: ' . get_script_uri() . '?' . rawurlencode($vars['page']));
 	exit;
