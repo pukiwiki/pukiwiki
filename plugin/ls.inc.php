0d27dd5dc59bcf57421463eae7d1156716879158
henoheno <henoheno>
2007-10-27 23:46:45 +0900

    Overhaul

--- plugin/ls.inc.php
+++ plugin/ls.inc.php
@@ -1,8 +1,8 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: ls.inc.php,v 1.10 2007/10/27 10:17:21 henoheno Exp $
+// $Id: ls.inc.php,v 1.11 2007/10/27 14:46:45 henoheno Exp $
 // Copyright (C)
-//   2002-2004 PukiWiki Developers Team
+//   2002-2004, 2007 PukiWiki Developers Team
 //   2002      Y.MASUI GPL2 http://masui.net/pukiwiki/ masui@masui.net
 // License: GPL version 2
 //
@@ -12,39 +12,41 @@ function plugin_ls_convert()
 {
 	global $vars;
 
+	$args = func_get_args();
+
 	$with_title = FALSE;
-
-	if (func_num_args())
-	{
-		$args = func_get_args();
-		$with_title = in_array('title',$args);
+	if (! empty($args)) {
+		$with_title = in_array('title', $args);
 	}
 
-	$prefix = $vars['page'].'/';
+	$page  = isset($vars['page']) ? $vars['page'] : '';
+	$pages = preg_grep('#^' .  preg_quote($page . '/' , '#') . '#', get_existpages());
 
-	$pages = array();
-	foreach (get_existpages() as $page)
-	{
-		if (strpos($page,$prefix) === 0)
-		{
-			$pages[] = $page;
-		}
-	}
 	natcasesort($pages);
 
 	$ls = array();
-	foreach ($pages as $page)
-	{
+	foreach ($pages as $page) {
 		$comment = '';
-		if ($with_title)
-		{
-			list($comment) = get_source($page);
-			// 見出しの固有ID部を削除
-			$comment = preg_replace('/^(\*{1,3}.*)\[#[A-Za-z][\w-]+\](.*)$/','$1$2',$comment);
 
-			$comment = '- ' . ereg_replace('^[-*]+','',$comment);
+		if ($with_title) {
+			$array = file_head(get_filename($page), 1);
+			if ($array) {
+				$comment = ' - ' .
+					preg_replace(
+						array(
+							'/^(\*{1,3}.*)\[#[A-Za-z][\w-]+\](.*)$/S',	// Remove fixed-heading anchors
+							'/^(?:-+|\*+)/',	// Remove syntax garbages at this situation
+						),
+						array(
+							'$1$2',
+							'',
+						),
+						current($array)
+					);
+			}
 		}
-		$ls[] = "-[[$page]] $comment";
+
+		$ls[] = '- [[' . $page . ']]' . $comment;
 	}
 
 	return convert_html($ls);
