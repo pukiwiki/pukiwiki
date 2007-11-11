3809644b6a3b69bd98de8ff214e3376e8c5a8d95
henoheno <henoheno>
2007-11-11 20:02:40 +0900

    * BugTrack2/285: & => &amp; * BugTrack/779: Not using list() and array_pad() * BugTrack/779: trim() * BugTrack/779: Simplify * Simplify: strip_htmltag() for AutoLink => strip_autolink() * Simplify: ruby tags

--- plugin/ruby.inc.php
+++ plugin/ruby.inc.php
@@ -1,29 +1,32 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: ruby.inc.php,v 1.6 2005/05/07 07:41:31 henoheno Exp $
+// $Id: ruby.inc.php,v 1.7 2007/11/11 11:02:40 henoheno Exp $
 //
-// Ruby annotation plugin: Add a pronounciation into kanji-word or acronym(s)
+// Ruby annotation plugin: Attach a pronounciation into kanji-word(s) or acronym(s)
 // See also about ruby: http://www.w3.org/TR/ruby/
 //
 // NOTE:
 //  Ruby tag works with MSIE only now,
 //  but readable for other browsers like: 'words(pronunciation)'
 
-define('PLUGIN_RUBY_USAGE', '&ruby(pronunciation){words};');
+define('PLUGIN_RUBY_USAGE', '&amp;ruby(pronunciation){words};');
 
 function plugin_ruby_inline()
 {
 	if (func_num_args() != 2) return PLUGIN_RUBY_USAGE;
 
-	list($ruby, $body) = func_get_args();
-
-	// strip_htmltag() is just for avoiding AutoLink insertion
-	$body = strip_htmltag($body);
+	$args = func_get_args();
+	$body = trim(strip_autolink(array_pop($args))); // htmlspecialchars() already
+	$ruby = isset($args[0]) ? trim($args[0]) : '';
 
 	if ($ruby == '' || $body == '') return PLUGIN_RUBY_USAGE;
 
-	return '<ruby><rb>' . $body . '</rb>' . '<rp>(</rp>' .
-		'<rt>' .  htmlspecialchars($ruby) . '</rt>' . '<rp>)</rp>' .
+	return 
+		'<ruby>' .
+			'<rb>' . $body . '</rb>' .
+			'<rp>(</rp>' .
+				'<rt>' .  htmlspecialchars($ruby) . '</rt>' .
+			'<rp>)</rp>' .
 		'</ruby>';
 }
 ?>
