6adf7b67d8ac18b5d9fb6b54ae9c8c0b1643f563
henoheno <henoheno>
2007-11-13 23:59:45 +0900

    BugTrack2/285: pxpx => px. intval()

--- plugin/size.inc.php
+++ plugin/size.inc.php
@@ -1,27 +1,35 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: size.inc.php,v 1.11 2007/11/11 12:09:57 henoheno Exp $
+// $Id: size.inc.php,v 1.12 2007/11/13 14:59:45 henoheno Exp $
 //
-// Text-size changing via CSS plugin
+// Font-size plugin
 //
 // See Also:
 // CCS 2.1 Specification: 15.7 Font size: the 'font-size' property
 // http://www.w3.org/TR/CSS21/fonts.html#propdef-font-size
 
+// Pixel
 define('PLUGIN_SIZE_PIXEL_DEFAULT', 12);
-
 define('PLUGIN_SIZE_PIXEL_MAX',     60);
 define('PLUGIN_SIZE_PIXEL_MIN',      8);
 
-// NOTE: PIXEL_DEFAULT and PIXEL_MAX limits these
-// (Consider &size(500%){&size(500%){TEXT};};)
+// Percentage
+// NOTE: PIXEL_DEFAULT, PIXEL_MAX and PIXEL_MIN limits this
+//   to suppress these tricks:
+//     &size(500%){&size(500%){TEXT};};  // Too big
+//     &size( 60%){&size( 60%){TEXT};};  // Too small
 define('PLUGIN_SIZE_PERCENT_MAX',  500);
 define('PLUGIN_SIZE_PERCENT_MIN',   60);
 
-define('PLUGIN_SIZE_REGEX', '/^(?:([0-9]+(?:px)?)|([0-9]+)%)$/i');
-
 // ----
+
 define('PLUGIN_SIZE_USAGE', '&amp;size(pixel or percentage){Text you want to change};');
+
+define('PLUGIN_SIZE_REGEX',
+	'/^(?:' .
+	'([0-9]+(?:\.[0-9]+)?)(?: *px)?' . '|' .	// Pixel (default)
+	'([0-9.]+(?:\.[0-9]+)?) *%' .				// Percentage
+	')$/i');
 
 function plugin_size_inline()
 {
@@ -34,19 +42,19 @@ function plugin_size_inline()
 	// strip_autolink() seems not needed for size plugin
 	//$body = strip_autolink($body);
 	
-	if ($size == '' || $body == '') {
-		return PLUGIN_SIZE_USAGE;
-	}
+	if ($size == '' || $body == '') return PLUGIN_SIZE_USAGE;
 
 	$matches = array();
 	if (preg_match(PLUGIN_SIZE_REGEX, $size, $matches)) {
 		if (isset($matches[2])) {
-			$percent = max(PLUGIN_SIZE_PERCENT_MIN, min(PLUGIN_SIZE_PERCENT_MAX, $matches[2]));
+			$percent = max(PLUGIN_SIZE_PERCENT_MIN,
+				min(PLUGIN_SIZE_PERCENT_MAX, intval($matches[2])));
 			$size = PLUGIN_SIZE_PIXEL_DEFAULT * $percent / 100;
  		} else {
  			$size = $matches[1];
  		}
-		$size = max(PLUGIN_SIZE_PIXEL_MIN, min(PLUGIN_SIZE_PIXEL_MAX, $size)) . 'px';
+		$size = max(PLUGIN_SIZE_PIXEL_MIN,
+			min(PLUGIN_SIZE_PIXEL_MAX, intval($size))) . 'px';
 	} else {
 		return PLUGIN_SIZE_USAGE;
 	}
