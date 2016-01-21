b5db2e96cba94b0f1fe1d7f88b08d0464bebd6ba
umorigu <umorigu@gmail.com>
2016-01-22 04:24:14 +0900

    BugTrack2/373 AUTH_TYPE_EXTERNAL - Use external sign-in mechanism
    In pukiwiki.ini.php, you can define $auth_external_login_url URL.
    It is a external sign in URL.
    
    For example,
    
    ```
    $auth_type = AUTH_TYPE_EXTERNAL;
    $auth_external_login_url = './exlogin.php';
    ```
    
    If you meet a private page that requires access permission,
    a system redirect you to './exlogin.php' with 'url_after_login'
    parameter.
    
    In this case, 'exlogin.php' is your custom PHP script.
    After you sign in your SSO system,
    please redirect back to 'url_after_login' page.

--- lib/auth.php
+++ lib/auth.php
@@ -182,6 +182,7 @@ function basic_auth($page, $auth_flag, $exit_flag, $auth_pages, $title_cannot)
 {
 	global $auth_method_type, $auth_users, $_msg_auth, $auth_user, $auth_groups;
 	global $auth_user_groups, $auth_type, $g_query_string;
+	global $auth_external_login_url;
 	// Checked by:
 	$target_str = '';
 	if ($auth_method_type == 'pagename') {
@@ -211,7 +212,14 @@ function basic_auth($page, $auth_flag, $exit_flag, $auth_pages, $title_cannot)
 			} elseif (AUTH_TYPE_FORM === $auth_type) {
 				$url_after_login = get_script_uri() . '?' . $g_query_string;
 				$loginurl = get_script_uri() . '?plugin=loginform'
-					. '&page=' . rawurlencode($page)
+					. '&page=' . pagename_urlencode($page)
+					. '&url_after_login=' . rawurlencode($url_after_login);
+				header('HTTP/1.0 302 Found');
+				header('Location: ' . $loginurl);
+			} elseif (AUTH_TYPE_EXTERNAL === $auth_type) {
+				$url_after_login = get_script_uri() . '?' . $g_query_string;
+				$loginurl = $auth_external_login_url . '?'
+					. '&page=' . pagename_urlencode($page)
 					. '&url_after_login=' . rawurlencode($url_after_login);
 				header('HTTP/1.0 302 Found');
 				header('Location: ' . $loginurl);
@@ -261,6 +269,7 @@ function ensure_valid_auth_user()
 			return true; // no auth input
 		}
 		case AUTH_TYPE_FORM:
+		case AUTH_TYPE_EXTERNAL:
 		{
 			session_start();
 			// session_regenerate_id(true);
