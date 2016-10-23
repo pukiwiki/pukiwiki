f77e9b1059274bcfd5a8b12040f2cd3a2f4620c5
umorigu <umorigu@gmail.com>
2016-10-24 02:39:22 +0900

    BugTrack/2367 Rename tdiary.css.php to tdiary.css
    Now CSS files have no PHP/Dynamic logic.

--- skin/tdiary.skin.php
+++ skin/tdiary.skin.php
@@ -575,7 +575,7 @@ if (isset($pkwk_dtd)) {
  <link rel="SHORTCUT ICON" href="<?php echo $image['favicon'] ?>" />
  <link rel="stylesheet" type="text/css" media="all" href="<?php echo SKIN_DIR ?>theme/base.css" />
  <link rel="stylesheet" type="text/css" media="all" href="<?php echo SKIN_DIR ?>theme/<?php echo $theme ?>/<?php echo $theme ?>.css" />
- <link rel="stylesheet" type="text/css" href="<?php echo SKIN_DIR ?>tdiary.css.php?color=<?php echo $css_theme ?>" />
+ <link rel="stylesheet" type="text/css" href="<?php echo SKIN_DIR ?>tdiary.css" />
 <?php if ($css_theme === 'black') { ?> <link rel="stylesheet" type="text/css" href="<?php echo SKIN_DIR ?>tdiary_black.css" /><?php } ?>
  <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php echo $link['rss'] ?>" /><?php // RSS auto-discovery ?>
 
