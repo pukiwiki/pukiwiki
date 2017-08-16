3b574739a7a05f735ed65496f20801b9300334a2
umorigu <umorigu@gmail.com>
2017-08-17 01:04:56 +0900

    BugTrack/2188 Define List layout by CSS instead of ini file
    * Define List styles by CSS instead of default.ini file
      * Add list-indent1, 2 and 3: that is express indent-distance to parent list
    * In default.ini.php, $_ul_margin, $_ol_margin and $_dl_margin must be 1
      * These are used as indent leaping level calclation source
      * $_list_pad_str = ' class="list%d list-indent%d"';
        * 2nd param of sprintf($_list_pad_str, $1, $2) is exactly indent-level.
    * This replacement from PHP logic to CSS is completely compatible
      with existing list logic that contains ls2 plugin
      and PukiWiki core list logic.

--- default.ini.php
+++ default.ini.php
@@ -1,8 +1,8 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: default.ini.php,v 1.25 2005/12/20 14:04:40 henoheno Exp $
-// Copyright (C)
-//   2003-2005 PukiWiki Developers Team
+// default.ini.php
+// Copyright
+//   2003-2017 PukiWiki Development Team
 //   2001-2002 Originally written by yu-ji
 // License: GPL v2 or (at your option) any later version
 //
@@ -30,14 +30,15 @@ $search_word_color = 1;
 $list_index = 1;
 
 /////////////////////////////////////////////////
-// リスト構造の左マージン
-$_ul_left_margin = 0;   // リストと画面左端との間隔(px)
-$_ul_margin = 16;       // リストの階層間の間隔(px)
-$_ol_left_margin = 0;   // リストと画面左端との間隔(px)
-$_ol_margin = 16;       // リストの階層間の間隔(px)
-$_dl_left_margin = 0;   // リストと画面左端との間隔(px)
-$_dl_margin = 16;        // リストの階層間の間隔(px)
-$_list_pad_str = ' class="list%d" style="padding-left:%dpx;margin-left:%dpx"';
+// Obsolete: リスト構造の左マージン (Leaves for compatibility of plugins)
+// Use list-indent1 CSS class instead
+$_ul_left_margin = 0;   // Must be 0 (Plugin backward compatibility)
+$_ul_margin = 1;        // Must be 1 (Plugin backward compatibility)
+$_ol_left_margin = 0;   // Must be 0
+$_ol_margin = 1;        // Must be 1
+$_dl_left_margin = 0;   // Must be 0
+$_dl_margin = 1;        // Must be 1
+$_list_pad_str = ' class="list%d list-indent%d"';
 
 /////////////////////////////////////////////////
 // テキストエリアのカラム数
@@ -178,5 +179,3 @@ $facemark_rules = array(
 	'&amp;(phoneto);'=>'[phoneto]',
 	'&amp;(faxto);'	=> '[faxto]',
 );
-
-?>
--- keitai.ini.php
+++ keitai.ini.php
@@ -1,8 +1,8 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: keitai.ini.php,v 1.25 2005/12/10 07:57:30 henoheno Exp $
-// Copyright (C)
-//   2002-2005 PukiWiki Developers Team
+// keitai.ini.php
+// Copyright
+//   2002-2017 PukiWiki Development Team
 //   2001-2002 Originally written by yu-ji
 // License: GPL v2 or (at your option) any later version
 //
@@ -30,13 +30,14 @@ $search_word_color = 0;
 $list_index = 0;
 
 /////////////////////////////////////////////////
-// リスト構造の左マージン
-$_ul_left_margin =  0;	// リストと画面左端との間隔(px)
-$_ul_margin      = 16;	// リストの階層間の間隔(px)
-$_ol_left_margin =  0;	// リストと画面左端との間隔(px)
-$_ol_margin      = 16;	// リストの階層間の間隔(px)
-$_dl_left_margin =  0;	// リストと画面左端との間隔(px)
-$_dl_margin      = 16;	// リストの階層間の間隔(px)
+// Obsolete: リスト構造の左マージン (Leaves for compatibility of plugins)
+// Use list-indent1 CSS class instead
+$_ul_left_margin =  0;	// Must be 0 (Plugin backward compatibility)
+$_ul_margin      = 1;	// Must be 1 (Plugin backward compatibility)
+$_ol_left_margin =  0;	// Must be 0
+$_ol_margin      = 1;	// Must be 1
+$_dl_left_margin =  0;	// Must be 0
+$_dl_margin      = 1;	// Must be 1
 $_list_pad_str   = '';
 
 /////////////////////////////////////////////////
@@ -383,5 +384,3 @@ switch ($ua_name) {
 }
 
 unset($matches, $ua_name, $ua_vers, $ua_agent, $special_rules);
-
-?>
